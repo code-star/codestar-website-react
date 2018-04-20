3a229fb180a4ada8697620922c69abf0a0186e78
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2018-04-20 12:11:20 +0200

    Not using Section Component in Intro, applying styles in component itself

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -2,45 +2,53 @@ import React from 'react';
 import logo from '../img/logo.svg';
 import CssBaseline from 'material-ui/CssBaseline';
 import Grid from 'material-ui/Grid';
-import Section from '../Section/Section';
-import styles from './Intro.module.css';
+import css from './Intro.module.css';
 import { Link } from 'react-router-dom';
 
-const Intro = () => (
-	<Section
-		container
-		id="intro"
-		className={styles.section}
-		backgroundVideoSrc="/2719001.mp4"
-	>
+import compose from 'recompose/compose';
+import { withStyles } from 'material-ui/styles';
+import withWidth from 'material-ui/utils/withWidth';
+
+const styles = theme => ({
+	section: {
+		[theme.breakpoints.up('md')]: {
+			minHeight: '100vh',
+		},
+	},
+});
+
+const Intro = props => (
+	<section id="intro" className={`${props.classes.section} ${css.section}`}>
 		<CssBaseline />
-		<div className={`container ${styles.container}`}>
+		<video autoPlay muted loop className={css.fullVideo}>
+			<source src="/2719001.mp4" type="video/mp4" />
+		</video>
+		<div className={`container ${css.containerCenter}`}>
 			<Grid container justify="center">
 				<Grid item xs={12} md={3}>
-					<img src={logo} alt="Logo" className={styles.logo} />
+					<img src={logo} alt="Logo" className={css.logo} />
 				</Grid>
 			</Grid>
 			<Grid container justify="center">
-				<Grid item xs={12} md={9} className={styles.grid}>
-					<p className={styles.p}>
+				<Grid item xs={12} md={9} className={css.grid}>
+					<p className={css.p}>
 						The digital world offers endless possibilities. The challenges are
 						often complex. We develop state-of-the-art software that’s simple to
 						use. Agile and productive, using the latest techniques. We program
 						with our hearts and with our minds, for organisations looking to
 						take the next step.
 					</p>
-					<p className={styles.p}>
+					<p className={css.p}>
 						We are the #1 partner for Full Stack Scala and Big Data solutions in
 						the Netherlands. We are Codestar.
 					</p>
 
-					<p className={styles.p}>
+					<p className={css.p}>
 						<Link to="/cases">Go to cases</Link>
 					</p>
 				</Grid>
 			</Grid>
 		</div>
-	</Section>
+	</section>
 );
-
-export default Intro;
+export default compose(withStyles(styles), withWidth())(Intro);
