2e4ee13d4a3a8e5ffaeb4368c3186749469ae219
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2018-04-20 17:33:03 +0200

    Video in background with full height minus navbar

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from '../img/logo.svg';
-import CssBaseline from 'material-ui/CssBaseline';
 import Grid from 'material-ui/Grid';
 import css from './Intro.module.css';
 import { Link } from 'react-router-dom';
@@ -10,45 +9,64 @@ import { withStyles } from 'material-ui/styles';
 import withWidth from 'material-ui/utils/withWidth';
 
 const styles = theme => ({
-	section: {
-		[theme.breakpoints.up('md')]: {
-			minHeight: '100vh',
+	fullHeightMinusNavBar: {
+		minHeight: 'calc(100vh - 56px)',
+		[theme.breakpoints.up('sm')]: {
+			minHeight: 'calc(100vh - 64px)',
 		},
 	},
 });
 
 const Intro = props => (
-	<section id="intro" className={`${props.classes.section} ${css.section}`}>
-		<CssBaseline />
-		<video autoPlay muted loop className={css.fullVideo}>
-			<source src="/2719001.mp4" type="video/mp4" />
-		</video>
-		<div className={`container ${css.containerCenter}`}>
-			<Grid container justify="center">
-				<Grid item xs={12} md={3}>
-					<img src={logo} alt="Logo" className={css.logo} />
+	<div>
+		<section
+			id="intro"
+			className={` ${css.bgVideoWrapper} ${
+				props.classes.fullHeightMinusNavBar
+			}`}
+		>
+			<video autoPlay muted loop className={css.fullVideo}>
+				<source src="/2719001.mp4" type="video/mp4" />
+			</video>
+			<div
+				className={`container ${css.containerCenter} ${
+					props.classes.fullHeightMinusNavBar
+				}`}
+			>
+				<Grid container justify="center">
+					<Grid item xs={12} md={3}>
+						<img src={logo} alt="Logo" className={css.logo} />
+					</Grid>
 				</Grid>
-			</Grid>
-			<Grid container justify="center">
-				<Grid item xs={12} md={9} className={css.grid}>
-					<p className={css.p}>
-						The digital world offers endless possibilities. The challenges are
-						often complex. We develop state-of-the-art software that’s simple to
-						use. Agile and productive, using the latest techniques. We program
-						with our hearts and with our minds, for organisations looking to
-						take the next step.
-					</p>
-					<p className={css.p}>
-						We are the #1 partner for Full Stack Scala and Big Data solutions in
-						the Netherlands. We are Codestar.
-					</p>
+				<Grid container justify="center">
+					<Grid item xs={12} md={9} className={css.grid}>
+						<p className={css.p}>
+							The digital world offers endless possibilities. The challenges are
+							often complex. We develop state-of-the-art software that’s simple
+							to use. Agile and productive, using the latest techniques. We
+							program with our hearts and with our minds, for organisations
+							looking to take the next step.
+						</p>
+						<p className={css.p}>
+							We are the #1 partner for Full Stack Scala and Big Data solutions
+							in the Netherlands. We are Codestar.
+						</p>
 
-					<p className={css.p}>
-						<Link to="/cases">Go to cases</Link>
-					</p>
+						<p className={css.p}>
+							<Link to="/cases">Go to cases</Link>
+						</p>
+					</Grid>
 				</Grid>
-			</Grid>
+			</div>
+		</section>
+		<div className="container">
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Error,
+				exercitationem vel. Exercitationem, illum magnam expedita adipisci quos
+				vel. Nemo nisi fugit odit cumque sunt soluta magnam. Laudantium itaque
+				nesciunt minima.
+			</p>
 		</div>
-	</section>
+	</div>
 );
 export default compose(withStyles(styles), withWidth())(Intro);
