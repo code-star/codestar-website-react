0f000bfd31617fafe11b5c75659c073875a1f964
Mikel Arnaiz <mikel.arnaiz@gmail.com>
2018-05-02 17:50:49 +0200

    OurStack Component

---
+++ src/OurStack/OurStack.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const OurStack = () => (
+	<div>
+		<h3>OUR CHOICE</h3>
+		<ul>
+			<li>Scala</li>
+			<li>Akka</li>
+			<li>Spray</li>
+			<li>Spark</li>
+			<li>Typescript</li>
+			<li>Docker</li>
+			<li>Mesos</li>
+			<li>AWS</li>
+			<li>Cassandra</li>
+			<li>Kafka</li>
+		</ul>
+	</div>
+);
+
+export default OurStack;
