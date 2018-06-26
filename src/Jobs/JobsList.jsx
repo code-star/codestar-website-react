87371780d55f12d5f472d979c01ed53e53b579ab
Hamza Haiken <tenchi@team2xh.net>
2018-06-26 14:47:09 +0200

    Add routes for job descriptions

---
+++ src/Jobs/JobsList.jsx
@@ -0,0 +1,24 @@
+const jobsList = [
+	{
+		name: 'Senior Scala Developer',
+		translation: 'job_scala',
+		path: 'scala',
+	},
+	{
+		name: 'Junior Scala Developer',
+		translation: 'job_scala_jr',
+		path: 'scala_jr',
+	},
+	{
+		name: 'Big Data Engineer',
+		translation: 'job_big_data',
+		path: 'big_data',
+	},
+	{
+		name: 'Front-End Developer',
+		translation: 'job_front_end',
+		path: 'front_end',
+	},
+];
+
+export default jobsList;
