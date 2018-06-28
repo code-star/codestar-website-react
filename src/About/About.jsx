0a0b4b9a0b9c0f5839ae903cda942c399a8e669e
Hamza Haiken <tenchi@team2xh.net>
2018-06-28 09:41:35 +0200

    Add more translations

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { translate } from 'react-i18next';
+import { Button } from 'material-ui';
+import Typography from 'material-ui/Typography';
+
 import Container from '../Container/Container';
 import Team from './Team';
 import OurStack from '../OurStack/OurStack';
 import Avatar from 'material-ui/Avatar';
-
-import { Button } from 'material-ui';
-import Typography from 'material-ui/Typography';
-import { Link } from 'react-router-dom';
 
 import css from './About.module.css';
 
@@ -18,73 +19,79 @@ function shuffleArray(array) {
 	return array;
 }
 
-const About = () => (
-	<div>
-		<section>
-			<Container marginTopNavBar>
-				<div className="row">
-					<div className="col">
-						<h3>WE PROGRAM</h3>
-						<p>
-							We love our job: Programming. At the end of the day, the code is
-							all that matters. We write that code. Clean code, that works. We
-							program Scala, but also Typescript, JavaScript and sometimes
-							Haskell. We’re multilingual and we’re not afraid to learn new
-							languages.
-						</p>
+@translate(['about'], { wait: true })
+class About extends Component {
+	render() {
+		const { t } = this.props;
+		return (
+			<div>
+				<section>
+					<Container marginTopNavBar>
+						<div className="row">
+							<div className="col">
+								<h3>{t('ABOUT_ATTRACT_TITLE')}</h3>
+								<p>{t('ABOUT_ATTRACT_TEXT')}</p>
 
-						<OurStack />
+								<OurStack />
 
-						<Typography variant="headline">The team</Typography>
-						<div className="d-flex justify-content-center flex-wrap">
-							{shuffleArray(Team)
-								.filter(
-									person =>
-										person && !person.gone && person.name && person.image
-								)
-								.map(person => (
-									<div className={css.avatarWrapper} key={person.image}>
-										<Typography variant="caption" className={css.tagline}>
-											{person.tagline}
-										</Typography>
-										<Avatar
-											alt={person.name}
-											title={person.name}
-											src={`${process.env.PUBLIC_URL}/images/team/${
-												person.image
-											}`}
-											className={`${css.avatar} m-auto`}
-										/>
-										<Typography variant="body1" className={`text-center mt-1`}>
-											{person.name} - {person.job}
-										</Typography>
-									</div>
-								))}
+								<Typography variant="headline">
+									{t('ABOUT_TEAM_TITLE')}
+								</Typography>
+								<div className="d-flex justify-content-center flex-wrap">
+									{shuffleArray(Team)
+										.filter(
+											person =>
+												person && !person.gone && person.name && person.image
+										)
+										.map(person => (
+											<div className={css.avatarWrapper} key={person.image}>
+												<Typography variant="caption" className={css.tagline}>
+													{person.tagline}
+												</Typography>
+												<Avatar
+													alt={person.name}
+													title={person.name}
+													src={`${process.env.PUBLIC_URL}/images/team/${
+														person.image
+													}`}
+													className={`${css.avatar} m-auto`}
+												/>
+												<Typography
+													variant="body1"
+													className={`text-center mt-1`}
+												>
+													{person.name} - {person.job}
+												</Typography>
+											</div>
+										))}
+								</div>
+
+								<h1>Gallery</h1>
+
+								<h1>Events</h1>
+								<h1>Jobs</h1>
+								<p>
+									Already in love? Check out{' '}
+									<Button component={Link} to="/jobs" color="inherit">
+										Our Vacancies
+									</Button>
+								</p>
+							</div>
 						</div>
+					</Container>
+				</section>
+				<section>
+					<Container fluid>
+						<div className="row">
+							<div className="col">
+								<h1>Photos</h1>
+							</div>
+						</div>
+					</Container>
+				</section>
+			</div>
+		);
+	}
+}
 
-						<h1>Gallery</h1>
-
-						<h1>Events</h1>
-						<h1>Jobs</h1>
-						<p>
-							Already in love? Check out{' '}
-							<Button component={Link} to="/jobs" color="inherit">
-								Our Vacancies
-							</Button>
-						</p>
-					</div>
-				</div>
-			</Container>
-		</section>
-		<section>
-			<Container fluid>
-				<div className="row">
-					<div className="col">
-						<h1>Photos</h1>
-					</div>
-				</div>
-			</Container>
-		</section>
-	</div>
-);
 export default About;
--- src/OurStack/OurStack.jsx
+++ src/OurStack/OurStack.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
 
-const OurStack = () => (
-	<div>
-		<h3>OUR CHOICE</h3>
-		<ul>
-			<li>Scala</li>
-			<li>Akka</li>
-			<li>Spray</li>
-			<li>Spark</li>
-			<li>Typescript</li>
-			<li>Docker</li>
-			<li>Mesos</li>
-			<li>AWS</li>
-			<li>Cassandra</li>
-			<li>Kafka</li>
-		</ul>
-	</div>
-);
+// TODO: Extract stack into list of objects
+@translate(['stack'], { wait: true })
+class OurStack extends Component {
+	render() {
+		const { t } = this.props;
+		return (
+			<div>
+				<h3>{t('STACK_TITLE')}</h3>
+				<ul>
+					<li>Scala</li>
+					<li>Akka</li>
+					<li>Spray</li>
+					<li>Spark</li>
+					<li>Typescript</li>
+					<li>Docker</li>
+					<li>Mesos</li>
+					<li>AWS</li>
+					<li>Cassandra</li>
+					<li>Kafka</li>
+				</ul>
+			</div>
+		);
+	}
+}
 
 export default OurStack;
