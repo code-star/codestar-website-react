52722c953550bbae8d55a350c12e97c9759396b5
Hamza Haiken <tenchi@team2xh.net>
2018-07-11 13:30:31 +0200

    Improve jobs page

--- src/Jobs/Jobs.jsx
+++ src/Jobs/Jobs.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'material-ui';
+import { translate } from 'react-i18next';
+import { withStyles } from 'material-ui/styles';
 
 import Container from '../Container/Container';
 import OurStack from '../OurStack/OurStack';
@@ -8,63 +10,79 @@ import Clients from '../Clients/Clients';
 import jobsList from './JobsList';
 import JobCard from './JobCard';
 
-const Jobs = () => (
-	<div>
-		<section className="py-3">
-			<Container marginTopNavBar>
-				<div className="row">
-					<div className="col">
-						<h3>THIS IS WHY YOU WANT TO WORK FOR US</h3>
-						<ul>
-							<li>
-								We’re not afraid to say it out loud: We’re very, very good
-							</li>
-							<li>We decide how we work</li>
-							<li>We’re never done learning</li>
-							<li>
-								We know the underlying principles of functional programming
-							</li>
-							<li>
-								We know the underlying principles of object-oriented programming
-							</li>
-							<li>
-								We share our passion at events and at international conferences
-							</li>
-						</ul>
+const styles = {
+	whiteText: {
+		color: 'white',
+	},
+	conduit: {
+		fontFamily: 'Conduit',
+		fontSize: '120%',
+	},
+};
 
-						<Clients title="Companies that trust on us" />
-
-						<OurStack />
-					</div>
-				</div>
-			</Container>
-		</section>
-		<section>
-			<Container>
-				<div className="row">
-					<div className="col">
-						<h3>Vacancies</h3>
-						<div className="d-flex justify-content-center flex-wrap">
-							{jobsList.map(job => (
-								<JobCard
-									translation={job.translation}
-									image={job.image}
-									path={job.path}
-								/>
-							))}
+@translate(['jobs'], { wait: true })
+class Jobs extends Component {
+	render() {
+		const { t, classes } = this.props;
+		return (
+			<div>
+				<section className="py-3">
+					<Container marginTopNavBar>
+						<div className="row">
+							<div className="col-12 mt-2 mb-4">
+								<h3 className={classes.whiteText}>{t('JOBS_VACANCIES')}</h3>
+								<div className="d-flex justify-content-center flex-wrap">
+									{jobsList.map(job => (
+										<JobCard
+											translation={job.translation}
+											image={job.image}
+											path={job.path}
+										/>
+									))}
+								</div>
+							</div>
 						</div>
+					</Container>
+				</section>
 
-						<p>
-							Not sure yet? Know more{' '}
-							<Button component={Link} to="/about" color="inherit">
-								About Us
-							</Button>
-						</p>
-					</div>
-				</div>
-			</Container>
-		</section>
-	</div>
-);
+				<section className="pb-3 pt-5" style={{ backgroundColor: '#eeeeee' }}>
+					<Container>
+						<div className="row mb-5">
+							<div className="col-12 col-md-6 align-self-center">
+								<h3>{t('JOBS_TITLE')}</h3>
+								<ul>
+									{t('JOBS_ARGUMENTS', { returnObjects: true }).map(
+										(arg, i) => <li key={i}>{arg}</li>
+									)}
+								</ul>
+							</div>
+							<div className="col-12 col-md-6">
+								<h3 className="mb-3">{t('JOBS_BEST_TECH')}</h3>
+								<OurStack />
+							</div>
+							<div className="col-12 mt-3">
+								<h3>{t('JOBS_BEST_CLIENTS')}</h3>
+								<Clients title="Companies that trust on us" />
+							</div>
+						</div>
+						<div className="row justify-content-center my-3">
+							<p>
+								Not sure yet? Know more{' '}
+								<Button
+									variant="raised"
+									component={Link}
+									to="/about"
+									color="inherit"
+								>
+									About Us
+								</Button>
+							</p>
+						</div>
+					</Container>
+				</section>
+			</div>
+		);
+	}
+}
 
-export default Jobs;
+export default withStyles(styles)(Jobs);
