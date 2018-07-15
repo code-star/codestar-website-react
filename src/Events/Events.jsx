4361d06e8406affc85f16843b105424cab37e4e2
M.D. van Es <es@mdworld.nl>
2018-07-15 10:28:34 +0200

    Events: Sanitize description and format as Cards

--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import { jsonp } from '../util';
 import i18n from '../i18n';
+import sanitizeHtml from 'sanitize-html';
+import {
+	Card,
+	CardMedia,
+	CardContent,
+	Typography,
+	CardActions,
+	Button,
+	withStyles,
+} from '@material-ui/core';
+import Container from '../Container/Container';
 
 // Meetup API test console: https://secure.meetup.com/meetup_api/console/?path=/:urlname/events
 // page=3 = number of results to return in a page, only need the first 3 results
@@ -12,34 +23,38 @@ const GET_PAST_EVENTS_URL =
 
 // TODO unit test
 
+/* TODO get cover image for each event.
+This url gets the event details, but is signed for one specific instance: https://api.meetup.com/Code-Star-Night/events/248958146?photo-host=public&sig_id=226887185&fields=featured_photo&sig=c634269c86bda35c0762874a490d219faba6365e
+Can use RxJS with JSONP? To combine streams?
+ */
+//const TEST_IMG_URL = "https://api.meetup.com/Code-Star-Night/events/248958146?photo-host=public&sig_id=226887185&fields=featured_photo&sig=c634269c86bda35c0762874a490d219faba6365e";
+
+const styles = {
+	card: {
+		maxWidth: 600, // 345,
+		marginBottom: '3em',
+	},
+	media: {
+		height: 0,
+		paddingTop: '56.25%', // 16:9
+	},
+};
+
 function convertEventResponseToModel(mEvent) {
+	// This works, but should be different for each mEvent
+	// jsonp(TEST_IMG_URL).then(response => {
+	// 	// TODO use _.get to guard against undefined?
+	// 	console.log(response.data.featured_photo.photo_link)
+	// });
+
 	return {
 		name: mEvent.name,
 		time: mEvent.time,
 		description: mEvent.description,
 		link: mEvent.link,
+		coverUrl:
+			'https://secure.meetupstatic.com/photos/event/c/1/4/3/600_469909475.jpeg',
 	};
-}
-
-// TODO safely parse description HTML
-function renderEventModel(mEvent) {
-	// TODO observe changes to i18n.language
-	const locale = i18n.language === 'nl' ? 'nl-NL' : 'en-US';
-	const formattedDate = new Date(mEvent.time).toLocaleDateString(locale, {
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-	});
-	return (
-		<div key={mEvent.time}>
-			<h3>
-				{formattedDate} - {mEvent.name}
-			</h3>
-			{mEvent.description}
-			<br />
-			<a href={mEvent.link}>read more</a>
-		</div>
-	);
 }
 
 export class Events extends Component {
@@ -49,8 +64,9 @@ export class Events extends Component {
 			nextEvents: [],
 			pastEvents: [],
 		};
-		// TODO is this called every time when navigating to this page or only once per session (should be the latter)?
+		this.renderEventModel = this.renderEventModel.bind(this);
 		this.fetchEvents = this.fetchEvents.bind(this);
+		// TODO is this called every time when navigating to this page or only once per session (should be the latter)?
 		this.fetchEvents();
 	}
 
@@ -70,17 +86,63 @@ export class Events extends Component {
 		});
 	}
 
+	renderEventModel(mEvent) {
+		const { classes } = this.props;
+		// TODO observe changes to i18n.language
+		const locale = i18n.language === 'nl' ? 'nl-NL' : 'en-US';
+		const formattedDate = new Date(mEvent.time).toLocaleDateString(locale, {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+		});
+		// Do stricter cleaning?
+		// const cleanDescription = sanitizeHtml(mEvent.description, {
+		// 	allowedTags: ['b', 'i', 'em', 'strong', 'a'],
+		// 	allowedAttributes: {
+		// 		a: ['href', 'target']
+		// 	}
+		// });
+		const cleanDescription = sanitizeHtml(mEvent.description);
+		return (
+			<Card key={mEvent.time} className={classes.card}>
+				<CardMedia
+					className={classes.media}
+					image={mEvent.coverUrl}
+					title={`${formattedDate} - ${mEvent.name}`}
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="headline" component="h2">
+						{formattedDate} - {mEvent.name}
+					</Typography>
+					<Typography
+						component="p"
+						dangerouslySetInnerHTML={{ __html: cleanDescription }}
+					/>
+				</CardContent>
+				<CardActions>
+					<Button size="small" color="primary" href={mEvent.link}>
+						Read More
+					</Button>
+				</CardActions>
+			</Card>
+		);
+	}
+
 	render() {
 		return (
 			<section>
-				<h1 style={{ marginTop: '100px' }}>EVENTS</h1>
-				<h2>Next Event</h2>
-				{this.state.nextEvents.map(renderEventModel)}
-				<h2>Past Events</h2>
-				{this.state.pastEvents.map(renderEventModel)}
+				<Container marginTopNavBar>
+					<h1 style={{ marginTop: '100px' }}>EVENTS</h1>
+					<h2>Next Event</h2>
+					{this.state.nextEvents.map(this.renderEventModel)}
+				</Container>
+				<Container className="mt-3">
+					<h2>Past Events</h2>
+					{this.state.pastEvents.map(this.renderEventModel)}
+				</Container>
 			</section>
 		);
 	}
 }
 
-export default Events;
+export default withStyles(styles)(Events);
