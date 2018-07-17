e65bf66c94e5f8f874f31a6392b1839aae4ed599
M.D. van Es <es@mdworld.nl>
2018-07-17 20:19:52 +0200

    Add Serverless

--- README.md
+++ README.md
@@ -14,12 +14,18 @@ Configured with
 These keys are stored under ~/.aws/credentials
 * yarn sls config credentials --provider aws --key YOUR_ACCESS_KEY_ID --secret YOUR_SECRET_ACCESS_KEY
 
+Default region is set in serverless.yml and can be added to sls with param `-r eu-west-1`
+
 Can also use npm without yarn, e.g.:
 * npx serverless create --template aws-nodejs --name static-site-mailer
 * npx sls config credentials --provider aws --key YOUR_ACCESS_KEY_ID --secret YOUR_SECRET_ACCESS_KEY
 
 Deploy to AWS with:
 * npx sls deploy --verbose
+This logs (among others) the POST endpoint, e.g. https://x.execute-api.us-east-1.amazonaws.com/dev/static-site-mailer
+This can be tested with Postman, but to call it from a form, CORS must be configured.
 
-And invoke with:
-* npx sls invoke --function staticSiteMailer
+And invoke with (--path is optional and points to a POST payload):
+* PROD: npx sls invoke --function staticSiteMailer --path data.json
+* DEV: npx sls invoke local --function staticSiteMailer --path data.json 
+
--- handler.js
+++ handler.js
@@ -1,16 +1,39 @@
 'use strict';
+const AWS = require('aws-sdk');
+const SES = new AWS.SES();
+
+function sendEmail(formData, callback) {
+	const emailParams = {
+		Source: '', // SES SENDING EMAIL  // TODO DO NOT COMMIT THIS
+		ReplyToAddresses: [formData.reply_to],
+		Destination: {
+			ToAddresses: [''], // TODO DO NOT COMMIT THIS
+		},
+		Message: {
+			Body: {
+				Text: {
+					Charset: 'UTF-8',
+					Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.reply_to}`,
+				},
+			},
+			Subject: {
+				Charset: 'UTF-8',
+				Data: 'New message from codestar.nl',
+			},
+		},
+	};
+
+	SES.sendEmail(emailParams, callback);
+}
 
 module.exports.staticSiteMailer = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+	const formData = JSON.parse(event.body);
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+	sendEmail(formData, function(err, data) {
+		if (err) {
+			console.log(err, err.stack);
+		} else {
+			console.log(data);
+		}
+	});
 };
