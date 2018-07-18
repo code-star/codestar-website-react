281242899277f410754a243a806f1c59991c3948
M.D. van Es <es@mdworld.nl>
2018-07-18 19:52:30 +0200

    Use Promise and send response

--- handler.js
+++ handler.js
@@ -2,39 +2,67 @@
 const AWS = require('aws-sdk');
 const SES = new AWS.SES();
 
-function sendEmail(formData, destinationAddress, callback) {
-	const emailParams = {
-		Source: destinationAddress, // SES SENDING EMAIL
-		ReplyToAddresses: [formData.reply_to],
-		Destination: {
-			ToAddresses: [ destinationAddress ],
-		},
-		Message: {
-			Body: {
-				Text: {
+function sendEmail(formData, destinationAddress) {
+	return new Promise((resolve, reject) => {
+		const emailParams = {
+			Source: destinationAddress, // SES SENDING EMAIL
+			ReplyToAddresses: [formData.reply_to],
+			Destination: {
+				ToAddresses: [ destinationAddress ],
+			},
+			Message: {
+				Body: {
+					Text: {
+						Charset: 'UTF-8',
+						Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.reply_to}`,
+					},
+				},
+				Subject: {
 					Charset: 'UTF-8',
-					Data: `${formData.message}\n\nName: ${formData.name}\nEmail: ${formData.reply_to}`,
+					Data: 'New message from codestar.nl',
 				},
 			},
-			Subject: {
-				Charset: 'UTF-8',
-				Data: 'New message from codestar.nl',
-			},
-		},
-	};
+		};
 
-	SES.sendEmail(emailParams, callback);
+		SES.sendEmail(emailParams, (err, data) => {
+			if (err) {
+				reject(err, err.stack);
+			} else {
+				resolve(data);
+			}
+		});
+	});
 }
+
+// Response headers
+const headers = {
+	'Content-Type': 'application/json',
+	'Access-Control-Allow-Origin': 'https://codestar.nl',
+};
 
 module.exports.staticSiteMailer = (event, context, callback) => {
 	const formData = JSON.parse(event.body);
 	const destinationAddress = process.env.STATIC_SITE_MAILER_DESTINATION;
 
-	sendEmail(formData, destinationAddress, function(err, data) {
-		if (err) {
+	sendEmail(formData, destinationAddress)
+		.then(data => {
+			//console.log(data)
+			callback(null, {
+				statusCode: 200,
+				headers,
+				body: JSON.stringify({
+					message: data,
+				}),
+			});
+		})
+		.catch(err => {
 			console.log(err, err.stack);
-		} else {
-			console.log(data);
-		}
-	});
+			callback(null, {
+				statusCode: 500,
+				headers,
+				body: JSON.stringify({
+					message: err.message,
+				}),
+			});
+		});
 };
