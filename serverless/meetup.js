7f719469d6e5a6c1096a5c810433775db1805ba3
M.D. van Es <es@mdworld.nl>
2018-07-24 19:22:00 +0200

    Disabled getUpcomingEvents for now

--- serverless/meetup.js
+++ serverless/meetup.js
@@ -17,53 +17,36 @@ const GET_UPCOMING_EVENTS_URL = 'https://api.meetup.com/Code-Star-Night/events?&
 module.exports.getUpcomingEvents = async (event, context, callback) => {
 	// TODO origin check
 
-	// TODO with await
-
-	const data = await fetch(GET_UPCOMING_EVENTS_URL, {
-		method: 'GET'
-	});
-	const resolvedData = await data.json();
-	// const ids = resolvedData.map(meetupEvent => {
+	// const data = await fetch(GET_UPCOMING_EVENTS_URL, {
+	// 	method: 'GET'
+	// });
+	// const resolvedData = await data.json();
+	// // const ids = resolvedData.map(meetupEvent => {
+	// // 	// TODO conditionally get event details, most importantly the image URL
+	// // 	console.log(meetupEvent.name);
+	// // 	return meetupEvent.id;
+	// // });
+	// const meetupEventPromises = resolvedData.map(meetupEvent => {
 	// 	// TODO conditionally get event details, most importantly the image URL
 	// 	console.log(meetupEvent.name);
-	// 	return meetupEvent.id;
+	// 	return fetch(`https://api.meetup.com/Code-Star-Night/events/${meetupEvent.id}?&sign=true&photo-host=public&fields=featured_photo`, {method: 'GET'});
 	// });
-	const meetupEventPromises = resolvedData.map(meetupEvent => {
-		// TODO conditionally get event details, most importantly the image URL
-		console.log(meetupEvent.name);
-		return fetch(`https://api.meetup.com/Code-Star-Night/events/${meetupEvent.id}?&sign=true&photo-host=public&fields=featured_photo`, {method: 'GET'});
-	});
-	const eventDetailsData = await meetupEventPromises[0]; // TODO convert to Promise.all
-	const eventDetailsDataResolved = await eventDetailsData.json();
-	console.log(eventDetailsDataResolved.featured_photo.photo_link);
-	// TODO with zip?
-	const newData = {
-		id: resolvedData[0].id,
-		name: resolvedData[0].name,
-		photo: eventDetailsDataResolved.featured_photo.photo_link
-	};
-	callback(null, {
-		statusCode: 200,
-		headers,
-		body: JSON.stringify({
-			message: newData,
-		}),
-	});
-
-	// fetch(GET_UPCOMING_EVENTS_URL, {
-	// 	method: 'GET'
-	// })
-	// 	.then(data => data.json())
-	// 	.then(data => {
-	// 		const ids = data.map(meetupEvent => {
-	// 			// TODO conditionally get event details, most importantly the image URL
-	// 			console.log(meetupEvent.name);
-	// 			return meetupEvent.id;
-	// 		});
-	// 		// console.log(promises);
-	// 		// return promises[0];
-	// 		return fetch(`https://api.meetup.com/Code-Star-Night/events/${ids[0]}?&sign=true&photo-host=public&fields=featured_photo`, {method: 'GET'});
-	// 	})
-	// 	.then(data => data.json())
-	// 	.then(data => console.log(data.featured_photo.photo_link))
+	// // TODO limit to e.g. 20
+	// // TODO show placeholders
+	// const eventDetailsData = await meetupEventPromises[0]; // TODO convert to Promise.all
+	// const eventDetailsDataResolved = await eventDetailsData.json();
+	// console.log(eventDetailsDataResolved.featured_photo.photo_link);
+	// // TODO with zip?
+	// const newData = {
+	// 	id: resolvedData[0].id,
+	// 	name: resolvedData[0].name,
+	// 	photo: eventDetailsDataResolved.featured_photo.photo_link
+	// };
+	// callback(null, {
+	// 	statusCode: 200,
+	// 	headers,
+	// 	body: JSON.stringify({
+	// 		message: newData,
+	// 	}),
+	// });
 };
