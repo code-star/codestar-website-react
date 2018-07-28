4416a586912292b635a959e0ad293e59b4b3bab0
M.D. van Es <es@mdworld.nl>
2018-07-28 16:24:46 +0200

    Mock Events in App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// TODO disabled because of UnhandledPromiseRejectionWarning, although the unit test passes
-// import App from './App';
+import App from './App';
 
-// it('renders without crashing', () => {
-// 	const div = document.createElement('div');
-// 	ReactDOM.render(<App />, div);
-// 	// ReactDOM.unmountComponentAtNode(div);
-// });
+// Mocked because of UnhandledPromiseRejectionWarning in Events
+jest.mock('./Events/Events', () => () => <div />);
 
-it('dummy', () => {});
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<App />, div);
+	// ReactDOM.unmountComponentAtNode(div);
+});
