72586fbf6bebbefbc4b1e39a73c6d4e5379fd77b
M.D. van Es <es@mdworld.nl>
2018-07-28 11:51:52 +0200

    Add minimal unit test for Events

---
+++ src/Events/Events.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Events } from './Events';
+
+jest.mock('react-i18next', () => ({
+	translate: () => Component => {
+		Component.defaultProps = { ...Component.defaultProps, t: () => '' };
+		return Component;
+	},
+}));
+
+jest.mock('../i18n', () => ({
+	language: 'nl',
+}));
+
+jest.mock('@material-ui/core/es/Hidden/Hidden', () => () => <div />);
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<Events />, div);
+	ReactDOM.unmountComponentAtNode(div);
+});
