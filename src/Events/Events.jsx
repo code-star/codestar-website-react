ba8de11b7b484427f7c7a7395361c01574fae605
M.D. van Es <es@mdworld.nl>
2018-07-29 09:23:06 +0200

    Extract EventsHeader from Events

--- src/Events/Events.jsx
+++ src/Events/Events.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { jsonp } from '../util';
 import i18n from '../i18n';
 import sanitizeHtml from 'sanitize-html';
@@ -10,13 +10,11 @@ import {
 	CardActions,
 	Button,
 	withStyles,
-	Hidden,
 } from '@material-ui/core';
 import Container from '../Container/Container';
 import _ from 'lodash';
-import ResponsiveImage from '../ResponsiveImage/ResponsiveImage';
-import { Link, Element } from 'react-scroll';
 import { translate } from 'react-i18next';
+import EventsHeader from '../EventsHeader/EventsHeader';
 
 /*
  TODO design ideas https://www.pixel-stitch.net/
@@ -96,7 +94,7 @@ export class Events extends Component {
 			pastEvents: [],
 		};
 		this.renderEventModel = this.renderEventModel.bind(this);
-		this.renderHeader = this.renderHeader.bind(this);
+		// this.renderHeader = this.renderHeader.bind(this);
 		this.fetchEvents = this.fetchEvents.bind(this);
 		// TODO is this called every time when navigating to this page or only once per session (should be the latter)?
 		this.fetchEvents();
@@ -170,164 +168,168 @@ export class Events extends Component {
 		);
 	}
 
-	renderHeader(mEvent) {
-		// TODO observe changes to i18n.language
-		const { t, classes } = this.props;
-		const locale = i18n.language === 'nl' ? 'nl-NL' : 'en-US';
-		const formattedDate = new Date(mEvent.time).toLocaleDateString(locale, {
-			year: 'numeric',
-			month: 'long',
-			day: 'numeric',
-		});
-		let descriptionElem = null;
-		if (mEvent.withDescription) {
-			const cleanDescription = sanitizeHtml(mEvent.description);
-			descriptionElem = (
-				<Typography
-					component="p"
-					dangerouslySetInnerHTML={{ __html: cleanDescription }}
-				/>
-			);
-		}
-		return (
-			<div key={mEvent.name}>
-				<section className={classes.section}>
-					<ResponsiveImage
-						path="/images/events/2017-09-28%20Andre%20Staltz%20RxJS.jpg"
-						asBackgroundImage
-						effect="e_art:fes"
-						alt="Andre Staltz presenting to a crowd at the Codestar Night meetup of September of 2018"
-					/>
-					<Container fullHeight center>
-						<div className="row">
-							<div className="col-12">
-								<div className="mt-4">
-									<Typography
-										align="center"
-										variant="display1"
-										style={{ color: 'white' }}
-									>
-										{t('OUR_NEXT_EVENT')}
-									</Typography>
-									<Hidden mdUp>
-										<Typography
-											align="center"
-											variant="display2"
-											className={classes.nextEventTitle}
-										>
-											{mEvent.name}
-										</Typography>
-									</Hidden>
-									<Hidden smDown>
-										<Typography
-											align="center"
-											variant="display4"
-											className={classes.nextEventTitle}
-										>
-											{mEvent.name}
-											{/*TODO: image should use filter, title, sign up button, buttons for the rest of the page, re-use style-color-white */}
-											{/*TODO image not 100% height*/}
-											{/*TODO event notification size must be bigger*/}
-										</Typography>
-									</Hidden>
-									<Typography
-										gutterBottom
-										align="center"
-										variant="display2"
-										style={{ color: 'white' }}
-									>
-										{formattedDate}
-									</Typography>
-									<div style={{ textAlign: 'center' }}>
-										{/*TODO conform button color to CaseHeader button color (purple)*/}
-										<Button
-											color="primary"
-											variant="raised"
-											href={mEvent.link}
-											className="mr-1"
-										>
-											{t('SIGN_UP')}
-										</Button>
-										<Link to="event-details" hashSpy smooth>
-											<Button variant="contained">{t('MORE_INFO')}</Button>
-										</Link>
-										{/*TODO stick buttons to offset from bottom*/}
-										{/*TODO instead of buttons links, use blocks like the grid on Cases*/}
-										{/*<div className="mt-5">
-											<Button
-												variant="contained"
-												className="mr-1"
-												href="#contained-buttons"
-											>
-												Previous Events
-											</Button>
-											<Button
-												variant="contained"
-												className="mr-1"
-												href="#contained-buttons"
-											>
-												Blog
-											</Button>
-											<Button
-												variant="contained"
-												className="mr-1"
-												href="#contained-buttons"
-											>
-												Photos
-											</Button>
-											<Button
-												variant="contained"
-												className="mr-1"
-												href="#contained-buttons"
-											>
-												Videos
-											</Button>
-										</div>*/}
-									</div>
-								</div>
-							</div>
-						</div>
-					</Container>
-				</section>
-				<Element name="event-details">
-					<section className="py-5 bg-white">
-						<Container center marginTopNavBar>
-							<div className="row">
-								<div className="col-12">
-									<Typography align="center" variant="title">
-										{mEvent.name}
-									</Typography>
-									<Typography gutterBottom align="center" variant="subheading">
-										{formattedDate}
-									</Typography>
-								</div>
-							</div>
-							<div className="row">
-								<div className="col-12 col-md-8">{descriptionElem}</div>
-								<div className="col-12 col-md-4">
-									<img
-										src={mEvent.coverUrl}
-										alt={`Artistic background with text "${mEvent.name}"`}
-										style={{ width: '100%' }}
-									/>
-									<Button
-										color="primary"
-										variant="raised"
-										href={mEvent.link}
-										className="mt-1"
-									>
-										{t('SIGN_UP')}
-									</Button>
-								</div>
-							</div>
-						</Container>
-					</section>
-				</Element>
-			</div>
-		);
-	}
+	// renderHeader(mEvent) {
+	// 	// TODO observe changes to i18n.language
+	// 	const { t, classes } = this.props;
+	// 	const locale = i18n.language === 'nl' ? 'nl-NL' : 'en-US';
+	// 	const formattedDate = new Date(mEvent.time).toLocaleDateString(locale, {
+	// 		year: 'numeric',
+	// 		month: 'long',
+	// 		day: 'numeric',
+	// 	});
+	// 	let descriptionElem = null;
+	// 	if (mEvent.withDescription) {
+	// 		const cleanDescription = sanitizeHtml(mEvent.description);
+	// 		descriptionElem = (
+	// 			<Typography
+	// 				component="p"
+	// 				dangerouslySetInnerHTML={{ __html: cleanDescription }}
+	// 			/>
+	// 		);
+	// 	}
+	// 	return (
+	// 		<div key={mEvent.name}>
+	// 			<section className={classes.section}>
+	// 				<ResponsiveImage
+	// 					path="/images/events/2017-09-28%20Andre%20Staltz%20RxJS.jpg"
+	// 					asBackgroundImage
+	// 					effect="e_art:fes"
+	// 					alt="Andre Staltz presenting to a crowd at the Codestar Night meetup of September of 2018"
+	// 				/>
+	// 				<Container fullHeight center>
+	// 					<div className="row">
+	// 						<div className="col-12">
+	// 							<div className="mt-4">
+	// 								<Typography
+	// 									align="center"
+	// 									variant="display1"
+	// 									style={{ color: 'white' }}
+	// 								>
+	// 									{t('OUR_NEXT_EVENT')}
+	// 								</Typography>
+	// 								<Hidden mdUp>
+	// 									<Typography
+	// 										align="center"
+	// 										variant="display2"
+	// 										className={classes.nextEventTitle}
+	// 									>
+	// 										{mEvent.name}
+	// 									</Typography>
+	// 								</Hidden>
+	// 								<Hidden smDown>
+	// 									<Typography
+	// 										align="center"
+	// 										variant="display4"
+	// 										className={classes.nextEventTitle}
+	// 									>
+	// 										{mEvent.name}
+	// 										{/*TODO: image should use filter, title, sign up button, buttons for the rest of the page, re-use style-color-white */}
+	// 										{/*TODO image not 100% height*/}
+	// 										{/*TODO event notification size must be bigger*/}
+	// 									</Typography>
+	// 								</Hidden>
+	// 								<Typography
+	// 									gutterBottom
+	// 									align="center"
+	// 									variant="display2"
+	// 									style={{ color: 'white' }}
+	// 								>
+	// 									{formattedDate}
+	// 								</Typography>
+	// 								<div style={{ textAlign: 'center' }}>
+	// 									{/*TODO conform button color to CaseHeader button color (purple)*/}
+	// 									<Button
+	// 										color="primary"
+	// 										variant="raised"
+	// 										href={mEvent.link}
+	// 										className="mr-1"
+	// 									>
+	// 										{t('SIGN_UP')}
+	// 									</Button>
+	// 									<Link to="event-details" hashSpy smooth>
+	// 										<Button variant="contained">{t('MORE_INFO')}</Button>
+	// 									</Link>
+	// 									{/*TODO stick buttons to offset from bottom*/}
+	// 									{/*TODO instead of buttons links, use blocks like the grid on Cases*/}
+	// 									{/*<div className="mt-5">
+	// 										<Button
+	// 											variant="contained"
+	// 											className="mr-1"
+	// 											href="#contained-buttons"
+	// 										>
+	// 											Previous Events
+	// 										</Button>
+	// 										<Button
+	// 											variant="contained"
+	// 											className="mr-1"
+	// 											href="#contained-buttons"
+	// 										>
+	// 											Blog
+	// 										</Button>
+	// 										<Button
+	// 											variant="contained"
+	// 											className="mr-1"
+	// 											href="#contained-buttons"
+	// 										>
+	// 											Photos
+	// 										</Button>
+	// 										<Button
+	// 											variant="contained"
+	// 											className="mr-1"
+	// 											href="#contained-buttons"
+	// 										>
+	// 											Videos
+	// 										</Button>
+	// 									</div>*/}
+	// 								</div>
+	// 							</div>
+	// 						</div>
+	// 					</div>
+	// 				</Container>
+	// 			</section>
+	// 			<Element name="event-details">
+	// 				<section className="py-5 bg-white">
+	// 					<Container center marginTopNavBar>
+	// 						<div className="row">
+	// 							<div className="col-12">
+	// 								<Typography align="center" variant="title">
+	// 									{mEvent.name}
+	// 								</Typography>
+	// 								<Typography gutterBottom align="center" variant="subheading">
+	// 									{formattedDate}
+	// 								</Typography>
+	// 							</div>
+	// 						</div>
+	// 						<div className="row">
+	// 							<div className="col-12 col-md-8">{descriptionElem}</div>
+	// 							<div className="col-12 col-md-4">
+	// 								<img
+	// 									src={mEvent.coverUrl}
+	// 									alt={`Artistic background with text "${mEvent.name}"`}
+	// 									style={{ width: '100%' }}
+	// 								/>
+	// 								<Button
+	// 									color="primary"
+	// 									variant="raised"
+	// 									href={mEvent.link}
+	// 									className="mt-1"
+	// 								>
+	// 									{t('SIGN_UP')}
+	// 								</Button>
+	// 							</div>
+	// 						</div>
+	// 					</Container>
+	// 				</section>
+	// 			</Element>
+	// 		</div>
+	// 	);
+	// }
 
 	render() {
+		const nextEvent =
+			this.state.nextEvents.length > 0 ? (
+				<EventsHeader MeetupEvent={this.state.nextEvents[0]} />
+			) : null;
 		const noEvents =
 			this.state.nextEvents.length === 0 ? (
 				<section className="py-5 bg-white">
@@ -346,9 +348,8 @@ export class Events extends Component {
 				</section>
 			) : null;
 		return (
-			<div>
-				{/*TODO replace div by Fragment*/}
-				{this.state.nextEvents.map(this.renderHeader)}
+			<Fragment>
+				{nextEvent}
 				{noEvents}
 				<section>
 					{/*<Container marginTopNavBar>
@@ -366,7 +367,7 @@ export class Events extends Component {
 						</div>
 					</Container>*/}
 				</section>
-			</div>
+			</Fragment>
 		);
 	}
 }
---
+++ src/EventsHeader/EventsHeader.jsx
@@ -0,0 +1,203 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import i18n from '../i18n';
+import sanitizeHtml from 'sanitize-html';
+import { Typography, Button, withStyles, Hidden } from '@material-ui/core';
+import Container from '../Container/Container';
+import ResponsiveImage from '../ResponsiveImage/ResponsiveImage';
+import { Link, Element } from 'react-scroll';
+import { translate } from 'react-i18next';
+
+const styles = {
+	section: {
+		position: 'relative',
+		overflow: 'hidden',
+	},
+	nextEventTitle: {
+		color: 'white',
+		fontWeight: 'bold',
+	},
+};
+
+@translate(['events'], { wait: true })
+export class EventsHeader extends Component {
+	constructor(props) {
+		super(props);
+		// this.state = {
+		// 	nextEvents: [],
+		// 	pastEvents: [],
+		// };
+		// this.renderEventModel = this.renderEventModel.bind(this);
+		this.renderDetailsSection = this.renderDetailsSection.bind(this);
+		// this.fetchEvents = this.fetchEvents.bind(this);
+		// // TODO is this called every time when navigating to this page or only once per session (should be the latter)?
+		// this.fetchEvents();
+	}
+
+	renderDetailsSection(mEvent, formattedDate, descriptionElem) {
+		const { t } = this.props;
+		return (
+			<Element name="event-details">
+				<section className="py-5 bg-white">
+					<Container center marginTopNavBar>
+						<div className="row">
+							<div className="col-12">
+								<Typography align="center" variant="title">
+									{mEvent.name}
+								</Typography>
+								<Typography gutterBottom align="center" variant="subheading">
+									{formattedDate}
+								</Typography>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-12 col-md-8">{descriptionElem}</div>
+							<div className="col-12 col-md-4">
+								<img
+									src={mEvent.coverUrl}
+									alt={`Artistic background with text "${mEvent.name}"`}
+									style={{ width: '100%' }}
+								/>
+								<Button
+									color="primary"
+									variant="raised"
+									href={mEvent.link}
+									className="mt-1"
+								>
+									{t('SIGN_UP')}
+								</Button>
+							</div>
+						</div>
+					</Container>
+				</section>
+			</Element>
+		);
+	}
+
+	render() {
+		const { MeetupEvent: mEvent, t, classes } = this.props;
+		// TODO observe changes to i18n.language
+		const locale = i18n.language === 'nl' ? 'nl-NL' : 'en-US';
+		const formattedDate = new Date(mEvent.time).toLocaleDateString(locale, {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+		});
+		const cleanDescription = sanitizeHtml(mEvent.description);
+		const descriptionElem = (
+			<Typography
+				component="p"
+				dangerouslySetInnerHTML={{ __html: cleanDescription }}
+			/>
+		);
+		return (
+			<Fragment>
+				<section className={classes.section}>
+					<ResponsiveImage
+						path="/images/events/2017-09-28%20Andre%20Staltz%20RxJS.jpg"
+						asBackgroundImage
+						effect="e_art:fes"
+						alt="Andre Staltz presenting to a crowd at the Codestar Night meetup of September of 2018"
+					/>
+					<Container fullHeight center>
+						<div className="row">
+							<div className="col-12">
+								<div className="mt-4">
+									<Typography
+										align="center"
+										variant="display1"
+										style={{ color: 'white' }}
+									>
+										{t('OUR_NEXT_EVENT')}
+									</Typography>
+									<Hidden mdUp>
+										<Typography
+											align="center"
+											variant="display2"
+											className={classes.nextEventTitle}
+										>
+											{mEvent.name}
+										</Typography>
+									</Hidden>
+									<Hidden smDown>
+										<Typography
+											align="center"
+											variant="display4"
+											className={classes.nextEventTitle}
+										>
+											{mEvent.name}
+											{/*TODO: image should use filter, title, sign up button, buttons for the rest of the page, re-use style-color-white */}
+											{/*TODO image not 100% height*/}
+											{/*TODO event notification size must be bigger*/}
+										</Typography>
+									</Hidden>
+									<Typography
+										gutterBottom
+										align="center"
+										variant="display2"
+										style={{ color: 'white' }}
+									>
+										{formattedDate}
+									</Typography>
+									<div style={{ textAlign: 'center' }}>
+										{/*TODO conform button color to CaseHeader button color (purple)*/}
+										<Button
+											color="primary"
+											variant="raised"
+											href={mEvent.link}
+											className="mr-1"
+										>
+											{t('SIGN_UP')}
+										</Button>
+										<Link to="event-details" hashSpy smooth>
+											<Button variant="contained">{t('MORE_INFO')}</Button>
+										</Link>
+										{/*TODO stick buttons to offset from bottom*/}
+										{/*TODO instead of buttons links, use blocks like the grid on Cases*/}
+										{/*<div className="mt-5">
+											<Button
+												variant="contained"
+												className="mr-1"
+												href="#contained-buttons"
+											>
+												Previous Events
+											</Button>
+											<Button
+												variant="contained"
+												className="mr-1"
+												href="#contained-buttons"
+											>
+												Blog
+											</Button>
+											<Button
+												variant="contained"
+												className="mr-1"
+												href="#contained-buttons"
+											>
+												Photos
+											</Button>
+											<Button
+												variant="contained"
+												className="mr-1"
+												href="#contained-buttons"
+											>
+												Videos
+											</Button>
+										</div>*/}
+									</div>
+								</div>
+							</div>
+						</div>
+					</Container>
+				</section>
+				{this.renderDetailsSection(mEvent, formattedDate, descriptionElem)}
+			</Fragment>
+		);
+	}
+}
+
+EventsHeader.propTypes = {
+	MeetupEvent: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(EventsHeader);
