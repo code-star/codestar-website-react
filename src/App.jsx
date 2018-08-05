554ae8cd24cd08792f1e535f4fa09f884a9eef14
M.D. van Es <es@mdworld.nl>
2018-08-05 11:32:25 +0200

    Moved Event notification service call to NavContainer

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,10 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 
 import theme from './codestarMuiTheme';
 import ScrollToTop from './ScrollToTop';
-import NavBar from './NavBar/NavBar';
-import SideMenu from './SideMenu/SideMenu';
 import Footer from './Footer/Footer';
 import jobsList from './Jobs/JobsList';
 import AsyncComponent from './AsyncComponent/AsyncComponent';
-import { jsonp } from './util';
+import NavContainer from './modules/NavContainer/NavContainer';
 
 function fullHeightAsyncComponent(C) {
 	return props => <AsyncComponent fullHeight component={() => C} {...props} />;
@@ -33,17 +31,7 @@ const AsyncEvents = fullHeightAsyncComponent(
 
 const sections = ['', 'cases', 'about', 'jobs', 'contact'];
 
-// Meetup API test console: https://secure.meetup.com/meetup_api/console/?path=/:urlname/events
-// page=3 = number of results to return in a page, only need the first 3 results
-const GET_UPCOMING_EVENTS_URL =
-	'https://api.meetup.com/Code-Star-Night/events?photo-host=secure&page=3&sig_id=226887185&status=upcoming&sig=e3efc6db037cf681181d84ae343459a36afbefd4';
-
 class App extends Component {
-	state = {
-		drawerMenu: false,
-		nextEvent: null,
-	};
-
 	constructor(props) {
 		super(props);
 		this.classes = props.classes;
@@ -55,23 +43,6 @@ class App extends Component {
 		this.updateBackgroundColor(this.history.location.pathname);
 	}
 
-	componentDidMount() {
-		this.fetchUpcomingEvent.call(this);
-	}
-
-	fetchUpcomingEvent() {
-		/* Meetup API only allows JSONP for client-side, non authenticated, api key signed GET requests.
-		   must use JSONP conform https://github.com/meetup/api/issues/211
-		   Fetch API does not support JSONP. no-cors mode creates an opaque response without data.
-		*/
-		jsonp(GET_UPCOMING_EVENTS_URL).then(data => {
-			const nextEvent = data.data[0];
-			if (nextEvent) {
-				this.setState({ nextEvent });
-			}
-		});
-	}
-
 	updateBackgroundColor(pathname) {
 		const section = pathname.split('/')[1];
 		const index = sections.indexOf(section);
@@ -79,26 +50,12 @@ class App extends Component {
 		document.body.style.backgroundPositionY = `${position}vh, 0`;
 	}
 
-	toggleDrawer = () => {
-		this.setState(prevState => {
-			return {
-				drawerMenu: !prevState.drawerMenu,
-			};
-		});
-	};
-
 	render() {
 		return (
 			<Router history={this.history}>
 				<MuiThemeProvider theme={theme}>
 					<CssBaseline />
-					<NavBar toggle={this.toggleDrawer} nextEvent={this.state.nextEvent} />
-					<SideMenu
-						open={this.state.drawerMenu}
-						toggle={this.toggleDrawer}
-						history={this.history}
-						nextEvent={this.state.nextEvent}
-					/>
+					<NavContainer history={this.history} />
 
 					<ScrollToTop>
 						<Switch>
