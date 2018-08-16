1baab4bea0c7209a052b3172832bb7a3f829dee1
M.D. van Es <es@mdworld.nl>
2018-08-16 14:23:28 +0200

    Log the values for CI

--- ci-build.sh
+++ ci-build.sh
@@ -1,8 +1,11 @@
-if [ ${TRAVIS_BRANCH} == develop ]; then
+#!/bin/bash
+echo TRAVIS_BRANCH is $TRAVIS_BRANCH
+if [ "${TRAVIS_BRANCH}" == develop ]; then
     REACT_APP_STAGE=test
 else
     REACT_APP_STAGE=prod
 fi
 
 export REACT_APP_STAGE
-npm run build
+echo REACT_APP_STAGE is $REACT_APP_STAGE
+# npm run build
