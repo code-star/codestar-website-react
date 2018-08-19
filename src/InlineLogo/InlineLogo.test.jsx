3f7974c377cbd50d8975e9a046ea69b5b1e4d47a
M.D. van Es <es@mdworld.nl>
2018-08-19 18:42:12 +0200

    Improve unit test for InlineLogo with snapshot

--- src/InlineLogo/InlineLogo.test.jsx
+++ src/InlineLogo/InlineLogo.test.jsx
@@ -15,27 +15,23 @@ it('renders without crashing', () => {
 });
 
 it('replaces Codestar in a string with an image', () => {
-	const comp = renderer.create(
-		<InlineLogo>
-			<div>test Codestar test</div>
-		</InlineLogo>
-	);
-	const compInstance = comp.getInstance();
-	const domResult = compInstance.render()[0].props.children;
-	expect(domResult[0]).toBe('test ');
-	// This is probably the easiest way to do duck-typing of the image that is injected instead of the string Codestar.
-	// It checks if the object that is injected has a "type" property that is a function.
-	expect(typeof domResult[1].type).toBe('function');
-	expect(domResult[2]).toBe(' test');
+	const comp = renderer
+		.create(
+			<InlineLogo>
+				<div>test Codestar test</div>
+			</InlineLogo>
+		)
+		.toJSON();
+	expect(comp).toMatchSnapshot();
 });
 
 it('leaves string without Codestar unchanged', () => {
-	const comp = renderer.create(
-		<InlineLogo>
-			<div>test test</div>
-		</InlineLogo>
-	);
-	const compInstance = comp.getInstance();
-	const domResult = compInstance.render()[0].props.children;
-	expect(domResult[0]).toBe('test test');
+	const comp = renderer
+		.create(
+			<InlineLogo>
+				<div>test test</div>
+			</InlineLogo>
+		)
+		.toJSON();
+	expect(comp).toMatchSnapshot();
 });
