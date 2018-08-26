8d913aaab2d6add6fe84667e3b42b518f2514b5e
M.D. van Es <es@mdworld.nl>
2018-08-26 12:47:35 +0200

    Replace proptypes by typing

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Container from '../../Container/Container';
 import Section from '../../Section/Section';
 import { translate } from '../../typed-translate';
 import EventsHeader from '../../EventsHeader/EventsHeader';
 import EventCard from '../../EventCard/EventCard';
+import compose from 'recompose/compose';
 
-type EventsProps = any;
-type EventsPropTypes = any;
+interface IEventProps {
+	t: any;
+	nextMeetupEvents: any[];
+	noNextMeetupEvent: boolean;
+	pastMeetupEvents: any[];
+}
 
 /*
  Suggestions for design concepts
@@ -16,15 +20,17 @@ type EventsPropTypes = any;
  https://colorlib.com/wp/free-event-website-templates/
 */
 
-// TODO replace propTypes by https://gist.github.com/wittydeveloper/5ffb5f7d5d0c744612404ffdc802cd0a
-@translate(['events'], { wait: true })
-export default class Events extends Component<EventsProps, EventsPropTypes> {
-	public static propTypes = {
-		nextMeetupEvents: PropTypes.array.isRequired,
-		noNextMeetupEvent: PropTypes.bool.isRequired,
-		pastMeetupEvents: PropTypes.array.isRequired,
-	};
+// const Events: any = ({
+// 											 t,
+// 											 nextEvent,
+// 											 pastEvents,
+// 										 }: {
+// 	t: any;
+// 	nextEvent: any;
+// 	pastEvents: any;
+// }) => {
 
+export class Events extends Component<IEventProps> {
 	public render() {
 		const {
 			t,
@@ -69,3 +75,59 @@ export default class Events extends Component<EventsProps, EventsPropTypes> {
 		);
 	}
 }
+
+export default compose(translate(['events'], { wait: true }))(Events);
+
+// import React, { Fragment } from 'react';
+// import Container from '../../Container/Container';
+// import Section from '../../Section/Section';
+// import EventsHeader from '../../EventsHeader/EventsHeader';
+// import EventCard from '../../EventCard/EventCard';
+// import compose from 'recompose/compose';
+// import { translate } from 'react-i18next';
+//
+// /*
+//  Suggestions for design concepts
+//  https://www.pixel-stitch.net/
+//  https://hencework.com/theme/mateve/music_concert/#
+//  https://colorlib.com/wp/free-event-website-templates/
+// */
+//
+// // TODO unit test
+//
+// const Events: any = ({
+// 											 t,
+// 											 nextEvent,
+// 											 pastEvents,
+// 										 }: {
+// 	t: any;
+// 	nextEvent: any;
+// 	pastEvents: any;
+// }) => {
+// 	const pastEventsList = pastEvents.map((mEvent: any) => (
+// 		<EventCard key={mEvent.time} MeetupEvent={mEvent} />
+// 	));
+// 	return (
+// 		<Fragment>
+// 			<EventsHeader data={nextEvent} />
+// 			<Section scrollname="previous-events">
+// 				<Container>
+// 					<h2 style={{ color: 'white' }}>{t('OUR_PREVIOUS_EVENTS')}</h2>
+// 					<div className="row">
+// 						<div className="d-flex justify-content-center flex-wrap">
+// 							{pastEventsList}
+// 						</div>
+// 					</div>
+// 				</Container>
+// 			</Section>
+// 		</Fragment>
+// 	);
+// };
+//
+// // TODO replace propTypes by https://gist.github.com/wittydeveloper/5ffb5f7d5d0c744612404ffdc802cd0a
+// // 	public static propTypes = {
+// // 		nextEvent: PropTypes.object.isRequired,
+// // 		pastEvents: PropTypes.array.isRequired,
+// // 	};
+//
+// export default compose(translate(['events'], { wait: true }))(Events);
