74b40a489bc5a62eddacfb562d33234cc051e5ec
M.D. van Es <es@mdworld.nl>
2018-08-26 14:12:09 +0200

    Extract HeaderContent

--- src/EventsHeader/EventsHeader.tsx
+++ src/EventsHeader/EventsHeader.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import i18n from '../i18n';
 import sanitizeHtml from 'sanitize-html';
-import { Typography, Button, withStyles, Hidden } from '@material-ui/core';
+import { Typography, Button, withStyles } from '@material-ui/core';
 import Container from '../Container/Container';
 import Section from '../Section/Section';
 import ResponsiveImage from '../ResponsiveImage/ResponsiveImage';
-import { Link } from 'react-scroll';
 import { translate, TranslationFunction } from 'react-i18next';
 import EventsHeaderButton from './EventsHeaderButton';
 import { navButtons } from './constants';
@@ -13,6 +12,7 @@ import { purple } from '@material-ui/core/colors';
 import css from './EventsHeader.module.css';
 import EventsHeaderMessage from './EventsHeaderMessage';
 import compose from 'recompose/compose';
+import HeaderContent from './HeaderContent';
 
 // TODO improve types by replacing "any"
 interface IEventsHeaderPropsInner {
@@ -99,64 +99,6 @@ export const EventsHeader = ({
 		);
 	};
 
-	const renderHeaderContent = (mEvent: any, formattedDate: string) => {
-		return (
-			<Container fullHeight center className="mt-5 mt-sm-2 mt-md-0">
-				<div className="row">
-					<div className="col-12">
-						<Typography
-							align="center"
-							variant="display1"
-							className={css.nextEventText}
-						>
-							{t('OUR_NEXT_EVENT')}
-						</Typography>
-						<Hidden mdUp>
-							<Typography
-								align="center"
-								variant="display2"
-								className={css.nextEventTitle}
-							>
-								{mEvent.name}
-							</Typography>
-						</Hidden>
-						<Hidden smDown>
-							<Typography
-								align="center"
-								variant="display4"
-								className={css.nextEventTitle}
-							>
-								{mEvent.name}
-							</Typography>
-						</Hidden>
-						<Typography
-							gutterBottom
-							align="center"
-							variant="display2"
-							className={css.nextEventText}
-						>
-							{formattedDate}
-						</Typography>
-						<div style={{ textAlign: 'center' }}>
-							<Button
-								color="primary"
-								variant="raised"
-								href={mEvent.link}
-								className={`mr-1 ${classes.button}`}
-							>
-								{t('SIGN_UP')}
-							</Button>
-							<Link to="event-details" hashSpy smooth>
-								<Button variant="contained">{t('MORE_INFO')}</Button>
-							</Link>
-							<div className="mt-5">{renderNavButtons()}</div>
-						</div>
-					</div>
-				</div>
-			</Container>
-		);
-	};
-
 	let headerContent = null;
 	let detailsSection = null;
 	const meetupEvent =
@@ -180,7 +122,11 @@ export const EventsHeader = ({
 				dangerouslySetInnerHTML={{ __html: cleanDescription }}
 			/>
 		);
-		headerContent = renderHeaderContent(meetupEvent, formattedDate);
+		headerContent = (
+			<HeaderContent mEvent={meetupEvent} formattedDate={formattedDate}>
+				{renderNavButtons()}
+			</HeaderContent>
+		);
 		detailsSection = renderDetailsSection(
 			meetupEvent,
 			formattedDate,
