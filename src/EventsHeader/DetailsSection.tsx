3cb8a6ad0050f347ad7edd834fddece8c0f224b6
M.D. van Es <es@mdworld.nl>
2018-08-26 14:25:35 +0200

    Extract DetailsSection

---
+++ src/EventsHeader/DetailsSection.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Typography, Button } from '@material-ui/core';
+import { translate, TranslationFunction } from 'react-i18next';
+import Container from '../Container/Container';
+import Section from '../Section/Section';
+import compose from 'recompose/compose';
+
+// TODO improve types by replacing "any"
+interface IDetailsSectionPropsInner {
+	t: TranslationFunction;
+	mEvent: any;
+	formattedDate: string;
+	descriptionElem: any;
+}
+
+interface IDetailsSectionPropsOuter {
+	mEvent: any;
+	formattedDate: string;
+	descriptionElem: any;
+}
+
+export const DetailsSection = ({
+	t,
+	mEvent,
+	formattedDate,
+	descriptionElem,
+}: IDetailsSectionPropsInner) => {
+	return (
+		<Section scrollname="event-details" className="bg-white">
+			<Container center>
+				<div className="row">
+					<div className="col-12">
+						<Typography align="center" variant="title">
+							{mEvent.name}
+						</Typography>
+						<Typography gutterBottom align="center" variant="subheading">
+							{formattedDate}
+						</Typography>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-12 col-md-8">{descriptionElem}</div>
+					<div className="col-12 col-md-4">
+						<img
+							src={mEvent.coverUrl}
+							alt={`Artistic background with text "${mEvent.name}"`}
+							style={{ width: '100%' }}
+						/>
+						<Button
+							color="primary"
+							variant="raised"
+							href={mEvent.link}
+							className="mt-1"
+						>
+							{t('SIGN_UP')}
+						</Button>
+					</div>
+				</div>
+			</Container>
+		</Section>
+	);
+};
+
+export default compose<IDetailsSectionPropsInner, IDetailsSectionPropsOuter>(
+	translate(['events'], { wait: true })
+)(DetailsSection);
--- src/EventsHeader/EventsHeader.tsx
+++ src/EventsHeader/EventsHeader.tsx
@@ -1,49 +1,25 @@
 import React from 'react';
 import i18n from '../i18n';
 import sanitizeHtml from 'sanitize-html';
-import { Typography, Button, withStyles } from '@material-ui/core';
-import Container from '../Container/Container';
-import Section from '../Section/Section';
+import { Typography } from '@material-ui/core';
 import ResponsiveImage from '../ResponsiveImage/ResponsiveImage';
-import { translate, TranslationFunction } from 'react-i18next';
 import EventsHeaderButton from './EventsHeaderButton';
 import { navButtons } from './constants';
-import { purple } from '@material-ui/core/colors';
 import css from './EventsHeader.module.css';
 import EventsHeaderMessage from './EventsHeaderMessage';
-import compose from 'recompose/compose';
 import HeaderContent from './HeaderContent';
+import DetailsSection from './DetailsSection';
 
 // TODO improve types by replacing "any"
-interface IEventsHeaderPropsInner {
-	t: TranslationFunction;
-	classes: Record<string, string>;
+interface IEventsHeaderProps {
 	nextMeetupEvents: any[];
 	noNextMeetupEvent: boolean;
 }
 
-interface IEventsHeaderPropsOuter {
-	nextMeetupEvents: any[];
-	noNextMeetupEvent: boolean;
-}
-
-const styles = (theme: any) => ({
-	button: {
-		color: theme.palette.getContrastText(purple[500]),
-		backgroundColor: purple[500],
-		'&:hover': {
-			color: 'white',
-			backgroundColor: purple[700],
-		},
-	},
-});
-
-export const EventsHeader = ({
-	t,
-	classes,
+const EventsHeader = ({
 	nextMeetupEvents,
 	noNextMeetupEvent,
-}: IEventsHeaderPropsInner) => {
+}: IEventsHeaderProps) => {
 	const renderNavButtons = () => {
 		return navButtons.map(config => {
 			return (
@@ -56,47 +32,6 @@ export const EventsHeader = ({
 				/>
 			);
 		});
-	};
-
-	const renderDetailsSection = (
-		mEvent: any,
-		formattedDate: string,
-		descriptionElem: any
-	) => {
-		return (
-			<Section scrollname="event-details" className="bg-white">
-				<Container center>
-					<div className="row">
-						<div className="col-12">
-							<Typography align="center" variant="title">
-								{mEvent.name}
-							</Typography>
-							<Typography gutterBottom align="center" variant="subheading">
-								{formattedDate}
-							</Typography>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-12 col-md-8">{descriptionElem}</div>
-						<div className="col-12 col-md-4">
-							<img
-								src={mEvent.coverUrl}
-								alt={`Artistic background with text "${mEvent.name}"`}
-								style={{ width: '100%' }}
-							/>
-							<Button
-								color="primary"
-								variant="raised"
-								href={mEvent.link}
-								className="mt-1"
-							>
-								{t('SIGN_UP')}
-							</Button>
-						</div>
-					</div>
-				</Container>
-			</Section>
-		);
 	};
 
 	let headerContent = null;
@@ -127,10 +62,12 @@ export const EventsHeader = ({
 				{renderNavButtons()}
 			</HeaderContent>
 		);
-		detailsSection = renderDetailsSection(
-			meetupEvent,
-			formattedDate,
-			descriptionElem
+		detailsSection = (
+			<DetailsSection
+				mEvent={meetupEvent}
+				formattedDate={formattedDate}
+				descriptionElem={descriptionElem}
+			/>
 		);
 	} else if (noNextMeetupEvent) {
 		headerContent = (
@@ -154,7 +91,4 @@ export const EventsHeader = ({
 	);
 };
 
-export default compose<IEventsHeaderPropsInner, IEventsHeaderPropsOuter>(
-	translate(['events'], { wait: true }),
-	withStyles(styles)
-)(EventsHeader);
+export default EventsHeader;
