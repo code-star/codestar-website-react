645fd9516d9b039c85592015f4d46cf3711ca01a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-08-29 12:04:55 +0200

    Refines Events to stateless functional component

--- src/components/Events/Events.tsx
+++ src/components/Events/Events.tsx
@@ -1,71 +1,58 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import Container from '../../Container/Container';
 import Section from '../../Section/Section';
-import { translate } from '../../typed-translate';
 import EventsHeader from '../../EventsHeader/EventsHeader';
 import EventCard from '../../EventCard/EventCard';
+import Heading from '../Common/Heading/Heading';
 
-type EventsProps = any;
-type EventsPropTypes = any;
-
-/*
- Suggestions for design concepts
- https://www.pixel-stitch.net/
- https://hencework.com/theme/mateve/music_concert/#
- https://colorlib.com/wp/free-event-website-templates/
-*/
-
-// TODO replace propTypes by https://gist.github.com/wittydeveloper/5ffb5f7d5d0c744612404ffdc802cd0a
-@translate(['events'], { wait: true })
-export default class Events extends Component<EventsProps, EventsPropTypes> {
-	public static propTypes = {
-		nextMeetupEvents: PropTypes.array.isRequired,
-		noNextMeetupEvent: PropTypes.bool.isRequired,
-		pastMeetupEvents: PropTypes.array.isRequired,
-	};
-
-	public render() {
-		const {
-			t,
-			nextMeetupEvents,
-			noNextMeetupEvent,
-			pastMeetupEvents,
-		} = this.props;
-		const nextEventsList = nextMeetupEvents.map(
-			({ description, withDescription, ...restOfEvent }: any) => (
-				// Strip the description and withDescription properties
-				<EventCard key={restOfEvent.time} MeetupEvent={...restOfEvent} />
-			)
-		);
-		const nextEventsBlock =
-			nextMeetupEvents && nextMeetupEvents.length > 0 ? (
-				<>
-					<h2 style={{ color: 'white' }}>{t('OUR_NEXT_EVENTS')}</h2>
-					<div className="row">
-						<div className="d-flex flex-wrap">{nextEventsList}</div>
-					</div>
-				</>
-			) : null;
-		const pastEventsList = pastMeetupEvents.map((mEvent: any) => (
-			<EventCard key={mEvent.time} MeetupEvent={mEvent} />
-		));
-		return (
-			<>
-				<EventsHeader
-					nextMeetupEvents={nextMeetupEvents}
-					noNextMeetupEvent={noNextMeetupEvent}
-				/>
-				<Section scrollname="previous-events">
-					<Container>
-						{nextEventsBlock}
-						<h2 style={{ color: 'white' }}>{t('OUR_PREVIOUS_EVENTS')}</h2>
-						<div className="row">
-							<div className="d-flex flex-wrap">{pastEventsList}</div>
-						</div>
-					</Container>
-				</Section>
-			</>
-		);
-	}
+interface IProps {
+	nextMeetupEvents: object[];
+	noNextMeetupEvent: boolean;
+	pastMeetupEvents: object[];
 }
+
+const Events = ({
+	nextMeetupEvents,
+	noNextMeetupEvent,
+	pastMeetupEvents,
+}: IProps) => {
+	return (
+		<>
+			<EventsHeader
+				nextMeetupEvents={nextMeetupEvents}
+				noNextMeetupEvent={noNextMeetupEvent}
+			/>
+			<Section scrollname="previous-events">
+				<Container>
+					{nextMeetupEvents && nextMeetupEvents.length > 0 ? (
+						<>
+							<Heading type="h2" text="OUR_NEXT_EVENTS" />
+							<div className="row">
+								<div className="d-flex flex-wrap">
+									{nextMeetupEvents.map(
+										({ description, withDescription, ...restOfEvent }: any) => (
+											<EventCard
+												key={restOfEvent.time}
+												MeetupEvent={...restOfEvent}
+											/>
+										)
+									)}
+								</div>
+							</div>
+						</>
+					) : null}
+					<Heading type="h2" text="OUR_PREVIOUS_EVENTS" />
+					<div className="row">
+						<div className="d-flex flex-wrap">
+							{pastMeetupEvents.map((mEvent: any) => (
+								<EventCard key={mEvent.time} MeetupEvent={mEvent} />
+							))}
+						</div>
+					</div>
+				</Container>
+			</Section>
+		</>
+	);
+};
+
+export default Events;
