772a045be9858e1d98f71dc92fe2c33d4c65f9aa
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-08-31 14:38:11 +0200

    Extends story structure

--- src/components/Common/Heading/Heading.stories.js
+++ src/components/Common/Heading/Heading.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, select, text } from '@storybook/addon-knobs';
 import Heading from './Heading.tsx';
 
-storiesOf('Components', module)
+storiesOf('Components/Text', module)
 	.addDecorator(withKnobs)
 	.add('Heading', () => {
 		const type = select('Type', ['h1', 'h2', 'h3', 'h4'], 'h2');
