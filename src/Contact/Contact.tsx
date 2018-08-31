2d0f772fa3ee864c5e7f8e78f3d085d7d0aea6d7
M.D. van Es <es@mdworld.nl>
2018-08-31 14:05:07 +0200

    Allow AWS test and prod stages

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -71,10 +71,11 @@ export class Contact extends React.Component<ContactProps, ContactState> {
 		if (hasMessageError) {
 			return;
 		}
+		const AWS_STAGE = process.env.REACT_APP_STAGE === 'test' ? 'test' : 'prod';
 		const url =
 			process.env.REACT_APP_STAGE === 'dev'
 				? '/mock/staticSiteMailer.json'
-				: 'https://2sif0durcj.execute-api.eu-west-1.amazonaws.com/dev/static-site-mailer';
+				: `https://2sif0durcj.execute-api.eu-west-1.amazonaws.com/${AWS_STAGE}/static-site-mailer`;
 		const options =
 			process.env.REACT_APP_STAGE === 'dev'
 				? {
--- src/eventsService.ts
+++ src/eventsService.ts
@@ -1,17 +1,18 @@
-const GET_UPCOMING_EVENTS_URL =
-	'https://2sif0durcj.execute-api.eu-west-1.amazonaws.com/dev/get-upcoming-events';
-const GET_PAST_EVENTS_URL =
-	'https://2sif0durcj.execute-api.eu-west-1.amazonaws.com/dev/get-past-events';
-
 let cachedUpcomingEvents: any[];
 let cachedPastEvents: any[];
 
+// This has been wrapped in a function to able to run unit tests where process.env.REACT_APP_STAGE is changed
+function getUrl(lambdaName: string) {
+	if (process.env.REACT_APP_STAGE === 'dev') {
+		return `/mock/${lambdaName}.json`;
+	}
+	const AWS_STAGE = process.env.REACT_APP_STAGE === 'test' ? 'test' : 'prod';
+	return `https://2sif0durcj.execute-api.eu-west-1.amazonaws.com/${AWS_STAGE}/${lambdaName}`;
+}
+
 async function fetchUpcomingEvents() {
 	try {
-		let url = GET_UPCOMING_EVENTS_URL;
-		if (process.env.REACT_APP_STAGE === 'dev') {
-			url = '/mock/getUpcomingEvents.json';
-		}
+		const url = getUrl('get-upcoming-events');
 		cachedUpcomingEvents = await fetch(url).then(data => data.json());
 		return cachedUpcomingEvents;
 	} catch (err) {
@@ -27,10 +28,7 @@ export async function getCachedUpcomingEvents() {
 
 async function fetchPastEvents() {
 	try {
-		let url = GET_PAST_EVENTS_URL;
-		if (process.env.REACT_APP_STAGE === 'dev') {
-			url = '/mock/getPastEvents.json';
-		}
+		const url = getUrl('get-past-events');
 		cachedPastEvents = await fetch(url).then(data => data.json());
 		return cachedPastEvents;
 	} catch (err) {
