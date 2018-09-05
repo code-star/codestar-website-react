2cafa636da38d429bce1d43c1306dbbc39cc40a1
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-05 10:06:52 +0200

    Alters references and applies linter rules

--- src/utility/font.js
+++ src/utility/font.js
@@ -1,10 +1,10 @@
 import iconPaths from '../fonts/selection.json';
 
 export const getPath = name => {
-	const icon = iconPaths.icons.find(icon => icon.properties.name === name);
+	const match = iconPaths.icons.find(icon => icon.properties.name === name);
 
-	if (icon) {
-		return icon.icon.paths.join(' ');
+	if (match) {
+		return match.icon.paths.join(' ');
 	} else {
 		console.warn(`icon ${name} does not exist.`);
 		return '';
