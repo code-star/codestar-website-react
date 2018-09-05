885b1d702cecf7dc2bbefc92de5bbfcbe6871b57
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-05 15:49:42 +0200

    Extends Icon props

--- src/components/Atoms/Image/Icon.stories.js
+++ src/components/Atoms/Image/Icon.stories.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select } from '@storybook/addon-knobs';
+import { withKnobs, select, text } from '@storybook/addon-knobs';
 import Icon from './Icon.tsx';
+import { EIcon } from '../../../utility/enumeration';
+
+const {
+	cheveronDown,
+	cheveronLeft,
+	cheveronRight,
+	cheveronUp,
+	heart,
+	swap,
+	bubble,
+	mail,
+	twitter,
+} = EIcon;
 
 storiesOf('Components/Atoms/Image', module)
 	.addDecorator(withKnobs)
 	.add('Icon', () => {
-		const name = select(
-			'Name',
-			[
-				'cheveron-down',
-				'cheveron-left',
-				'cheveron-right',
-				'cheveron-up',
-				'heart',
-				'swap',
-				'bubble',
-				'mail',
-				'twitter',
-			],
-			'twitter'
-		);
+		const knobs = {
+			name: select(
+				'Name',
+				[
+					cheveronDown,
+					cheveronLeft,
+					cheveronRight,
+					cheveronUp,
+					heart,
+					swap,
+					bubble,
+					mail,
+					twitter,
+				],
+				'twitter'
+			),
+			text: text('Text', 7),
+		};
 
-		return <Icon name={name} width={20} height={20} />;
+		return <Icon name={knobs.name} width={20} height={20} text={knobs.text} />;
 	});
--- src/components/Atoms/Image/Icon.tsx
+++ src/components/Atoms/Image/Icon.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import style from './Icon.module.css';
 import { getPath } from '../../../utility/font';
+
+const cx = classNames.bind(style);
 
 interface IIconProps {
 	name: string;
@@ -7,6 +11,7 @@ interface IIconProps {
 	height: number;
 	fill?: string;
 	className?: string;
+	text?: string;
 }
 
 const Icon = ({
@@ -15,17 +20,21 @@ const Icon = ({
 	height,
 	fill = '',
 	className = '',
+	text = '',
 }: IIconProps) => (
-	<svg
-		fill={fill}
-		fontSize="1px"
-		width={width}
-		height={height}
-		viewBox="0 0 1024 1024"
-		className={className}
-	>
-		<path d={getPath(name)} />
-	</svg>
+	<div className={cx('icon')}>
+		<svg
+			fill={fill}
+			fontSize="1px"
+			width={width}
+			height={height}
+			viewBox="0 0 1024 1024"
+			className={className}
+		>
+			<path d={getPath(name)} />
+		</svg>
+		<span>{text}</span>
+	</div>
 );
 
 export default Icon;
