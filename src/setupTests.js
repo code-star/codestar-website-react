2488b52b923638b5b5998cb5c8bbd7ef488d0869
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-06 14:22:01 +0200

    Extends test setup with Enzyme and global renderToJSON

--- src/setupTests.js
+++
@@ -1,4 +0,0 @@
-// Needed to solve - Warning: React depends on requestAnimationFrame. Make sure that you load a polyfill in older browsers. https://fb.me/react-polyfills
-global.requestAnimationFrame = callback => {
-	setTimeout(callback, 0);
-};
