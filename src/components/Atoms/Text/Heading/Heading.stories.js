1bdf9312721d70807ace92ea2196ef26bb0c6ea9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-07 12:19:55 +0200

    Lints Heading

--- src/components/Atoms/Text/Heading/Heading.stories.js
+++ src/components/Atoms/Text/Heading/Heading.stories.js
@@ -4,9 +4,9 @@ import { withKnobs, select } from '@storybook/addon-knobs';
 import Heading from './Heading.tsx';
 
 storiesOf('Components/Atoms/Text', module)
-	.addDecorator(withKnobs)
-	.add('Heading', () => {
-		const type = select('Type', ['h1', 'h2', 'h3', 'h4'], 'h2');
+  .addDecorator(withKnobs)
+  .add('Heading', () => {
+    const type = select('Type', ['h1', 'h2', 'h3', 'h4'], 'h2');
 
-		return <Heading type={type} text={`Awesome ${type} heading`} />;
-	});
+    return <Heading type={type} text={`Awesome ${type} heading`} />;
+  });
--- src/components/Atoms/Text/Heading/Heading.test.js
+++ src/components/Atoms/Text/Heading/Heading.test.js
@@ -4,38 +4,38 @@ import { shallow } from 'enzyme';
 import Heading from './Heading';
 
 const getComponent = type => (
-	<Heading type={type} text={`Awesome ${type} heading`} />
+  <Heading type={type} text={`Awesome ${type} heading`} />
 );
 
 const renderShallow = () => {
-	return shallow(getComponent('h1'));
+  return shallow(getComponent('h1'));
 };
 
 describe('<Heading />', () => {
-	let wrapper;
+  let wrapper;
 
-	describe('Instance', () => {
-		test('must be an instance of Heading', () => {
-			wrapper = renderShallow();
-			expect(wrapper.find('Heading')).toBeTruthy();
-		});
-	});
+  describe('Instance', () => {
+    test('must be an instance of Heading', () => {
+      wrapper = renderShallow();
+      expect(wrapper.find('Heading')).toBeTruthy();
+    });
+  });
 
-	describe('Snaphot', () => {
-		test('must match h1', () => {
-			expect(global.renderToJSON(getComponent('h1'))).toMatchSnapshot();
-		});
+  describe('Snaphot', () => {
+    test('must match h1', () => {
+      expect(global.renderToJSON(getComponent('h1'))).toMatchSnapshot();
+    });
 
-		test('must match h2', () => {
-			expect(global.renderToJSON(getComponent('h2'))).toMatchSnapshot();
-		});
+    test('must match h2', () => {
+      expect(global.renderToJSON(getComponent('h2'))).toMatchSnapshot();
+    });
 
-		test('must match h3', () => {
-			expect(global.renderToJSON(getComponent('h3'))).toMatchSnapshot();
-		});
+    test('must match h3', () => {
+      expect(global.renderToJSON(getComponent('h3'))).toMatchSnapshot();
+    });
 
-		test('must match h4', () => {
-			expect(global.renderToJSON(getComponent('h4'))).toMatchSnapshot();
-		});
-	});
+    test('must match h4', () => {
+      expect(global.renderToJSON(getComponent('h4'))).toMatchSnapshot();
+    });
+  });
 });
