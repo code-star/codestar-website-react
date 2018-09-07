1e6312838db0ae49325e47fdd114f25d5670fed3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-07 12:18:48 +0200

    Lints Icon

--- src/components/Atoms/Image/Icon/Icon.stories.js
+++ src/components/Atoms/Image/Icon/Icon.stories.js
@@ -5,38 +5,38 @@ import Icon from './Icon.tsx';
 import { EIcon } from '../../../../utility/enumeration';
 
 const {
-	cheveronDown,
-	cheveronLeft,
-	cheveronRight,
-	cheveronUp,
-	heart,
-	swap,
-	bubble,
-	mail,
-	twitter,
+  cheveronDown,
+  cheveronLeft,
+  cheveronRight,
+  cheveronUp,
+  heart,
+  swap,
+  bubble,
+  mail,
+  twitter,
 } = EIcon;
 
 storiesOf('Components/Atoms/Image', module)
-	.addDecorator(withKnobs)
-	.add('Icon', () => {
-		const knobs = {
-			name: select(
-				'Name',
-				[
-					cheveronDown,
-					cheveronLeft,
-					cheveronRight,
-					cheveronUp,
-					heart,
-					swap,
-					bubble,
-					mail,
-					twitter,
-				],
-				'twitter'
-			),
-			text: text('Text', 7),
-		};
+  .addDecorator(withKnobs)
+  .add('Icon', () => {
+    const knobs = {
+      name: select(
+        'Name',
+        [
+          cheveronDown,
+          cheveronLeft,
+          cheveronRight,
+          cheveronUp,
+          heart,
+          swap,
+          bubble,
+          mail,
+          twitter,
+        ],
+        'twitter'
+      ),
+      text: text('Text', 7),
+    };
 
-		return <Icon name={knobs.name} width={20} height={20} text={knobs.text} />;
-	});
+    return <Icon name={knobs.name} width={20} height={20} text={knobs.text} />;
+  });
--- src/components/Atoms/Image/Icon/Icon.tsx
+++ src/components/Atoms/Image/Icon/Icon.tsx
@@ -6,35 +6,35 @@ import { getPath } from '../../../../utility/font';
 const cx = classNames.bind(style);
 
 interface IIconProps {
-	name: string;
-	width: number;
-	height: number;
-	fill?: string;
-	className?: string;
-	text?: string;
+  name: string;
+  width: number;
+  height: number;
+  fill?: string;
+  className?: string;
+  text?: string;
 }
 
 const Icon = ({
-	name,
-	width,
-	height,
-	fill = '',
-	className = '',
-	text = '',
+  name,
+  width,
+  height,
+  fill = '',
+  className = '',
+  text = '',
 }: IIconProps) => (
-	<div className={cx('icon')}>
-		<svg
-			fill={fill}
-			fontSize="1px"
-			width={width}
-			height={height}
-			viewBox="0 0 1024 1024"
-			className={className}
-		>
-			<path d={getPath(name)} />
-		</svg>
-		<span>{text}</span>
-	</div>
+  <div className={cx('icon')}>
+    <svg
+      fill={fill}
+      fontSize="1px"
+      width={width}
+      height={height}
+      viewBox="0 0 1024 1024"
+      className={className}
+    >
+      <path d={getPath(name)} />
+    </svg>
+    <span>{text}</span>
+  </div>
 );
 
 export default Icon;
