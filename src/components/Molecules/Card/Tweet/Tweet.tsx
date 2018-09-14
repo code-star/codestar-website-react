fca1f23111190f77f4132a56c3ae3e8c4d4cdc11
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-09-14 15:04:19 +0200

    Refines Tweetcomponent

--- src/components/Molecules/Card/Tweet/Tweet.tsx
+++ src/components/Molecules/Card/Tweet/Tweet.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import style from './Tweet.module.css';
 import Label from '../../../Atoms/Text/Label';
-import Icon from '../../../Atoms/Image/Icon';
-import { EColor, ESize, EIcon } from '../../../../utility/enumeration';
+import { EColor, ESize } from '../../../../utility/enumeration';
 
 const cx = classNames.bind(style);
 
@@ -33,28 +32,9 @@ const Tweet = ({
         size={ESize.medium}
         text={new Date(createdAt).toDateString()}
       />
-      <Icon name={EIcon.cheveronDown} width={20} height={20} />
     </div>
     <div className={cx('tweetBody')}>
       <Label color={EColor.black} size={ESize.small} text={text} />
-    </div>
-    <div className={cx('tweetFooter')}>
-      <Icon name={EIcon.bubble} width={18} height={18} fill="#000" />
-      <Icon
-        name={EIcon.swap}
-        width={18}
-        height={18}
-        fill="#000"
-        text={String(retweetCount)}
-      />
-      <Icon
-        name={EIcon.heart}
-        width={18}
-        height={18}
-        fill="#000"
-        text={String(favoriteCount)}
-      />
-      <Icon name={EIcon.mail} width={18} height={18} fill="#000" />
     </div>
   </div>
 );
