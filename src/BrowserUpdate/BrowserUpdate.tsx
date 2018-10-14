41631bd955a56f9519c59fe2a4789bc9c497b998
M.D. van Es <es@mdworld.nl>
2018-10-14 11:02:06 +0200

    BrowserUpdate show condition

--- src/BrowserUpdate/BrowserUpdate.tsx
+++ src/BrowserUpdate/BrowserUpdate.tsx
@@ -1,30 +1,21 @@
-import React, { SFC } from 'react';
-import { withStateHandlers } from 'recompose';
+import React, { SFC, Fragment } from 'react';
 import styles from './BrowserUpdate.module.css';
-import compose from 'recompose/compose';
 
-interface IPropsInner {
-  isHidden: boolean;
-}
-
-export const BrowserUpdate: SFC<IPropsInner> = () => (
-  <div className={styles.browserUpdate}>
-    <p>Please upgrade your browser.</p>
-    <p>
-      <a href="https://browser-update.org/update-browser.html">
-        More information
-      </a>
-    </p>
-  </div>
-);
-
-export const initialState = { isHidden: false };
-export const stateUpdaters = {
-  // Example of state argument with typing: handleMouseOver: ({ isHovering } : { isHovering: boolean}) => () => ({ isHovering: true }),
-  // handleMouseOver: () => () => ({ isHovering: true }),
-  // handleMouseOut: () => () => ({ isHovering: false }),
+const BrowserUpdate: SFC = () => {
+  // Very basic feature detection that excludes IE11
+  const showUpdateMessage = !window.fetch;
+  return showUpdateMessage ? (
+    <div className={styles.browserUpdate}>
+      <p>Please upgrade your browser.</p>
+      <p>
+        <a href="https://browser-update.org/update-browser.html">
+          More information
+        </a>
+      </p>
+    </div>
+  ) : (
+    <Fragment />
+  );
 };
 
-export default compose<IPropsInner, {}>(
-  withStateHandlers(initialState, stateUpdaters)
-)(BrowserUpdate);
+export default BrowserUpdate;
