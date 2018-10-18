32ade153e9c4a2278013da8640bbd851eeb9769b
M.D. van Es <es@mdworld.nl>
2018-10-18 15:30:18 +0200

    Fix unit test for browser update in fixture DOM

--- src/components/Molecules/BrowserUpdate/BrowserUpdateFallback.test.ts
+++ src/components/Molecules/BrowserUpdate/BrowserUpdateFallback.test.ts
@@ -6,24 +6,22 @@ jest.useFakeTimers();
 
 describe('addBrowserUpdate()', () => {
   beforeEach(() => {
-    document.body.innerHTML = '<div id="fixture"></div>';
+    document.body.innerHTML = '<div id="dummy-content"></div>';
   });
 
   describe('Snaphot', () => {
     it('shows message if Map.prototype.keys not defined', () => {
       const originalMapKeys = globalAny.Map.prototype.keys;
       delete Map.prototype.keys;
-      expect(addBrowserUpdate('fixture')).toMatchSnapshot();
+      expect(addBrowserUpdate('dummy-content')).toMatchSnapshot();
       jest.advanceTimersByTime(1000);
-      // TODO should not be empty fixture
-      expect(document.getElementById('fixture')).toMatchSnapshot();
+      expect(document.body).toMatchSnapshot();
       globalAny.Map.prototype.keys = originalMapKeys;
     });
 
     it('does not show message if Map.prototype.keys is defined', () => {
-      expect(addBrowserUpdate('fixture')).toMatchSnapshot();
-      // TODO should not be empty fixture
-      expect(document.getElementById('fixture')).toMatchSnapshot();
+      expect(addBrowserUpdate('dummy-content')).toMatchSnapshot();
+      expect(document.body).toMatchSnapshot();
     });
   });
 });
