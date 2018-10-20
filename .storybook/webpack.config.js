f565f28ee25d4ef77f9d41a7ba7fac349e769852
M.D. van Es <es@mdworld.nl>
2018-10-20 15:52:05 +0200

    Allow scss in storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,6 +5,16 @@ module.exports = (baseConfig, env, config) => {
     test: /\.(ts|tsx)$/,
     loader: require.resolve("awesome-typescript-loader")
   });
+
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      "style-loader",
+      "css-loader",
+      "sass-loader"
+    ]
+  });
+
   config.plugins.push(new TSDocgenPlugin());
   config.resolve.extensions.push(".ts", ".tsx");
   return config;
