d9687b94db9325b7e3483c50f5732b843f1d35ca
M.D. van Es <es@mdworld.nl>
2018-11-17 13:24:35 +0100

    Extract firstTweetArr

--- src/components/Molecules/List/TweetList/TweetList.tsx
+++ src/components/Molecules/List/TweetList/TweetList.tsx
@@ -26,56 +26,63 @@ const TweetList = ({
   eventImage,
   eventName,
   children,
-}: ITweetListProps) => (
-  <Card raised className={styles.card}>
-    {tweets.slice(0, 1).map(tweet => (
-      <CardHeader
-        key={tweet.id}
-        avatar={
-          <Avatar
-            src={tweet.user.profile_image_url_https}
-            aria-label="Speaker Twitter Avatar"
-          />
-        }
-        title={tweet.user.name}
-        subheader={eventDate}
-      />
-    ))}
-    <CardMedia image={eventImage} className={styles.media} title={eventName} />
-    <CardContent>
-      {tweets.map(tweet => (
-        <div key={tweet.id} className="p-2">
-          <a
-            href={`https://twitter.com/${tweet.user.screen_name}/status/${
-              tweet.id_str
-            }`}
-          >
-            <Typography component="div" className={styles.prefix}>
-              <img
-                src={getResponsiveImageUrl('/images/events/twitter', 30)}
-                className="mr-2"
-              />
-              {new Date(tweet.created_at).toDateString()}
-            </Typography>
-            <Typography component="p">{tweet.text}</Typography>
-          </a>
-        </div>
-      ))}
-    </CardContent>
-    <CardActions>
-      {children}
-      {tweets.slice(0, 1).map(tweet => (
-        <Button
+}: ITweetListProps) => {
+  const firstTweetArr = tweets.slice(0, 1);
+  return (
+    <Card raised className={styles.card}>
+      {firstTweetArr.map(tweet => (
+        <CardHeader
           key={tweet.id}
-          size="small"
-          color="secondary"
-          href={`https://twitter.com/${tweet.user.screen_name}?lang=en`}
-        >
-          About {tweet.user.name}
-        </Button>
+          avatar={
+            <Avatar
+              src={tweet.user.profile_image_url_https}
+              aria-label="Speaker Twitter Avatar"
+            />
+          }
+          title={tweet.user.name}
+          subheader={eventDate}
+        />
       ))}
-    </CardActions>
-  </Card>
-);
+      <CardMedia
+        image={eventImage}
+        className={styles.media}
+        title={eventName}
+      />
+      <CardContent>
+        {tweets.map(tweet => (
+          <div key={tweet.id} className="p-2">
+            <a
+              href={`https://twitter.com/${tweet.user.screen_name}/status/${
+                tweet.id_str
+              }`}
+            >
+              <Typography component="div" className={styles.prefix}>
+                <img
+                  src={getResponsiveImageUrl('/images/events/twitter', 30)}
+                  className="mr-2"
+                />
+                {new Date(tweet.created_at).toDateString()}
+              </Typography>
+              <Typography component="p">{tweet.text}</Typography>
+            </a>
+          </div>
+        ))}
+      </CardContent>
+      <CardActions>
+        {children}
+        {firstTweetArr.map(tweet => (
+          <Button
+            key={tweet.id}
+            size="small"
+            color="secondary"
+            href={`https://twitter.com/${tweet.user.screen_name}?lang=en`}
+          >
+            About {tweet.user.name}
+          </Button>
+        ))}
+      </CardActions>
+    </Card>
+  );
+};
 
 export default TweetList;
