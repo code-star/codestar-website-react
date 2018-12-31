6f7d276f439adc4bc7966151f59c4e2ca3807d83
M.D. van Es <es@mdworld.nl>
2018-12-31 11:26:40 +0100

    Add site and publications sections

--- src/components/Pages/About/About.tsx
+++ src/components/Pages/About/About.tsx
@@ -1,67 +1,143 @@
-import * as React from 'react';
+import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
-import { translate } from 'react-i18next';
-import { Button, Typography } from '@material-ui/core';
+import { translate, TranslationFunction } from 'react-i18next';
+import {Button, Theme, Typography, createStyles} from '@material-ui/core';
+import compose from 'recompose/compose';
+import grey from '@material-ui/core/colors/grey';
+import blue from '@material-ui/core/colors/blue';
 import Container from '../../../Container/Container';
 import OurStack from '../../../OurStack/OurStack';
 import TeamCarousel from '../../Molecules/TeamCarousel/TeamCarousel';
-import styles from './About.module.scss';
+import Section from '../../Molecules/Section/Section';
+import css from './About.module.scss';
+import { withStyles } from '@material-ui/core/styles';
 
-type AboutProps = any;
+interface IPropsInner {
+  classes: any;
+  t: TranslationFunction;
+}
+
+interface IPropsOuter {}
+
+const styles = (theme: Theme) => createStyles({
+  teamSection: {
+    backgroundColor: grey[200]
+  },
+  siteSection: {
+    backgroundColor: blue[900],
+    color: grey[200]
+  }
+});
 
 // Fixme: this is a workaround for using the material ui button
 // with the `to` property. By default this is not supported.
 const CustomButton = (props: any) => <Button {...props} />;
 
-class About extends React.Component<AboutProps> {
-  public render() {
-    const { t } = this.props;
-    return (
-      <div>
-        <section className="py-5">
-          <Container marginTopNavBar>
-            <div className="row justify-content-center">
-              <div className="col-12 col-md-8">
-                <h3 className={styles.whiteText}>{t('ABOUT_ATTRACT_TITLE')}</h3>
-                <p className={[styles.whiteText, styles.conduit].join(' ')}>
-                  {t('ABOUT_ATTRACT_TEXT')}
-                </p>
-              </div>
-              <div className="col-12 col-md-8 col-sd-6">
-                <OurStack />
-              </div>
+export const About: FC<IPropsInner> = ({ t, classes }) => {
+  return (
+    <>
+      <Section scrollname={"attract"} className="py-5">
+        <Container marginTopNavBar>
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-8">
+              <h3 className={css.whiteText}>{t('ABOUT_ATTRACT_TITLE')}</h3>
+              <p className={[css.whiteText, css.conduit].join(' ')}>
+                {t('ABOUT_ATTRACT_TEXT')}
+              </p>
             </div>
-            <div className="row justify-content-center py-3">
-              {/*Already in love? Check out{' '}*/}
-              <CustomButton
-                className="mt-3"
-                variant="contained"
-                component={Link}
-                to={'/jobs'}
-                color="inherit"
-              >
-                {t('ABOUT_VACANCIES')}
-              </CustomButton>
+            <div className="col-12 col-md-8 col-sd-6">
+              <OurStack />
             </div>
-          </Container>
-        </section>
-        <section className="py-5" style={{ background: '#eeeeee' }}>
-          <Container>
-            <div className="row">
-              <div className="col">
-                <Typography variant="display1" align="center" className="mb-3">
-                  {t('ABOUT_TEAM_TITLE')}
-                </Typography>
-              </div>
-            </div>
-          </Container>
-          <div className="row m-0">
-            <TeamCarousel />
           </div>
-        </section>
-      </div>
-    );
-  }
-}
+          <div className="row justify-content-center py-3">
+            {/*Already in love? Check out{' '}*/}
+            <CustomButton
+              className="mt-3"
+              variant="contained"
+              component={Link}
+              to={'/jobs'}
+              color="inherit"
+            >
+              {t('ABOUT_VACANCIES')}
+            </CustomButton>
+          </div>
+        </Container>
+      </Section>
+      <Section scrollname={"team"} className={`py-5 ${classes.teamSection}`} >
+        <Container>
+          <div className="row">
+            <div className="col">
+              <Typography variant="h4" align="center" className="mb-3">
+                {t('ABOUT_TEAM_TITLE')}
+              </Typography>
+            </div>
+          </div>
+        </Container>
+        <div className="row m-0">
+          <TeamCarousel />
+        </div>
+      </Section>
+      <Section
+        scrollname={"site"}
+        className={`py-5 ${classes.siteSection}`}
+      >
+        <Container>
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-8">
+              <Typography variant="h4" align="center" className="mb-3">
+                {t('SITE_TITLE')}
+              </Typography>
+              <Typography variant="body1" className="mb-3">
+                Bij Codestar werken we met veel plezier bij klanten, en
+                daarnaast zorgen we er voor dat we elke vrijdag bij elkaar zijn
+                om kennis uit te wisselen en contact te houden. In deze tijd
+                werken we aan projecten, maar ook aan R&D. Op die manier is deze
+                site ook ontstaan: als showcase voor de stack waar wij mee
+                (willen) werken. We hebben hiervoor onder andere gebruik gemaakt
+                van CRA, TypeScript, React, Storybook, CSS Modules, AWS Lamdba
+                en Jest. En natuurlijk is het open source. Kijk in de{' '}
+                <a href="https://github.com/code-star/codestar-website-react">
+                  repo
+                </a>{' '}
+                voor meer details.
+              </Typography>
+            </div>
+          </div>
+        </Container>
+      </Section>
+      <Section
+        scrollname={"publications"}
+        className="py-5"
+      >
+        <Container>
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-8">
+              <Typography variant="h4" align="center" className="mb-3">
+                {t('PUBLICATIONS_TITLE')}
+              </Typography>
+              <Typography variant="body1" className="mb-3">
+                Bij Codestar werken we met veel plezier bij klanten, en
+                daarnaast zorgen we er voor dat we elke vrijdag bij elkaar zijn
+                om kennis uit te wisselen en contact te houden. In deze tijd
+                werken we aan projecten, maar ook aan R&D. Op die manier is deze
+                site ook ontstaan: als showcase voor de stack waar wij mee
+                (willen) werken. We hebben hiervoor onder andere gebruik gemaakt
+                van CRA, TypeScript, React, Storybook, CSS Modules, AWS Lamdba
+                en Jest. En natuurlijk is het open source. Kijk in de{' '}
+                <a href="https://github.com/code-star/codestar-website-react">
+                  repo
+                </a>{' '}
+                voor meer details.
+              </Typography>
+            </div>
+          </div>
+        </Container>
+      </Section>
+    </>
+  );
+};
 
-export default translate(['about'], { wait: true })(About);
+export default compose<IPropsInner, IPropsOuter>(
+  withStyles(styles),
+  translate(['about'], { wait: true })
+)(About);
