eca1924129c5d8dc315c13a84c916f725526d0e7
Martin van Dam <martin.van.dam@ordina.nl>
2019-01-31 11:37:46 +0100

    Convert AsyncComponent to TypeScript

---
+++ src/AsyncComponent/AsyncComponent.tsx
@@ -0,0 +1,57 @@
+import React, { Component, SFC, CSSProperties, ComponentType } from 'react';
+
+type AsyncComponentProps = Readonly<{
+  fullHeight: boolean;
+  component: () => Promise<{
+    default: ComponentType;
+  }>;
+}>;
+
+type AsyncComponentState = Readonly<{
+  component: false | ComponentType;
+}>;
+
+export default class AsyncComponent extends Component<
+  AsyncComponentProps,
+  AsyncComponentState
+> {
+  constructor(props: AsyncComponentProps) {
+    super(props);
+
+    this.state = {
+      component: false,
+    };
+  }
+
+  public async componentDidMount() {
+    const { default: component } = await this.props.component();
+    this.setState({
+      component,
+    });
+  }
+
+  public render() {
+    const Component = this.state.component;
+
+    if (!Component) {
+      return null;
+    }
+
+    if (this.props.fullHeight) {
+      return (
+        <FullHeight>
+          <Component {...this.props} />
+        </FullHeight>
+      );
+    }
+
+    return <Component {...this.props} />;
+  }
+}
+
+const FullHeight: SFC<{}> = props => {
+  const styles: CSSProperties = {
+    minHeight: '100vh',
+  };
+  return <div style={styles}>{props.children}</div>;
+};
