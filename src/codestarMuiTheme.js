6155c9dcd300f1466b600e5c7e52dc7e4fac2d72
M.D. van Es <es@mdworld.nl>
2019-03-15 20:51:26 +0100

    Convert Sass to Mui Theme

--- src/codestarMuiTheme.js
+++ src/codestarMuiTheme.js
@@ -1,8 +1,23 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+// import blue from '@material-ui/core/colors/blue';
 
 const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    },
+  },
   typography: {
-    //fontFamily: ['Conduit'].join(','),
+    // fontFamily: 'Conduit'
   },
 });
 
