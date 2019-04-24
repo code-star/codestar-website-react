a2c53f07411b98ec004ba616e6479acdffd31ba3
M.D. van Es <es@mdworld.nl>
2019-04-24 21:01:21 +0200

    Fix unit test snapshot

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-
+import renderer from "react-test-renderer";
 import App from './App';
-import renderToJSON from "./setupTests";
 
 jest.mock('./ScrollToTop', () => 'scroll-to-top');
 jest.mock('./Footer/Footer', () => 'footer');
@@ -11,42 +9,9 @@ jest.mock('./Jobs/JobsList', () => {
 });
 jest.mock('./containers/NavContainer/NavContainer', () => 'nav-container');
 
-const renderShallow = () => {
-  return shallow(<App />);
-};
-
-const renderMount = () => {
-  return mount(<App />);
-};
-
 describe('<App />', () => {
-  let renderSpy: jest.SpyInstance;
-
-  beforeEach(() => {
-    renderSpy = jest.spyOn(App.prototype, 'render');
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  describe('Component Lifecycle', () => {
-    it('must call render', () => {
-      renderShallow();
-      expect(renderSpy).toHaveBeenCalledTimes(1);
-    });
-  });
-
-  describe('Instance', () => {
-    it('must have an instance of App', () => {
-      const wrapper = renderMount();
-      expect(wrapper.find('App')).toHaveLength(1);
-    });
-  });
-
-  describe('Snaphot', () => {
-    it('default', () => {
-      expect(renderToJSON(renderShallow().get(0))).toMatchSnapshot();
-    });
+  it('renders the App', () => {
+    const comp = renderer.create(<App />).toJSON();
+    expect(comp).toMatchSnapshot();
   });
 });
